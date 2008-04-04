dd9f78052e48bc76d94975ae3ec45c72dc0f3426
gilles <gilles@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2008-04-04 13:26:26 +0000

    added version
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk@239 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/core/log/Version.java
@@ -0,0 +1,23 @@
+package xtremweb.core.log;
+
+/**
+ * Describe class Version here.
+ *
+ *
+ * Created: Fri Mar 28 12:36:30 2008
+ *
+ * @author <a href="mailto:fedak@xtremciel.local">Gilles Fedak</a>
+ * @version 1.0
+ */
+public class Version {
+
+    public final static int major = 0;
+    public final static int minor = 0;
+    public final static int revision = 1;
+    public final static String branch = "";
+
+    public static String versionToString() {
+	return "" + major + "." + minor + "." + revision + ((branch.equals(""))?"":("_"+branch));
+    }
+
+}
