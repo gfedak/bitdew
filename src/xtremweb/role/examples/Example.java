44582d4f28ed7dcbbbf4010b61b173833bb60953
gilles <gilles@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2008-04-07 07:56:06 +0000

    added common framework for all example files
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk@244 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/role/examples/Example.java
@@ -0,0 +1,19 @@
+package xtremweb.role.examples;
+
+/**
+ * Describe class Example here.
+ *
+ *
+ * Created: Sat Apr  5 09:34:23 2008
+ *
+ * @author <a href="mailto:fedak@xtremciel.gillus.net">Gilles Fedak</a>
+ * @version 1.0
+ */
+public  class Example {
+
+
+
+
+    public static void main(String[] args) throws Exception {
+    }
+}
