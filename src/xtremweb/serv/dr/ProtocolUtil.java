6df4d95c68df79b30b2c6abf0cd23b265a5f4f83
gilles <gilles@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2010-01-07 23:08:09 +0000

    preparing version 0.2.3
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@383 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/serv/dr/ProtocolUtil.java
+++ src/xtremweb/serv/dr/ProtocolUtil.java
@@ -18,11 +18,11 @@ public class ProtocolUtil {
 
     public static String toString(Protocol proto) {
 	String tmp =  "proto " + proto.getname() + " [" + proto.getuid() + "] = { ";
-	if (proto.getserver" type="String" desc="Server name"/>
-	     <Attribute  name="port" type="int" desc="Port to contact"/>
-<Attribute  name="login" type="String" desc="Login name (if necessary)"/>
-<Attribute  name="password" type="String" desc="Password (if necessary)"/>
-<Attribute  name="path" type="String" desc="Path (if necessary)"/>
+	if (proto.getserver() != null) tmp += "server" + proto.getserver() + ", " ;
+	if (proto.getport() != 0) tmp += "port" + proto.getport() + ", ";
+	if (proto.getlogin() != null) tmp += "port" + proto.getlogin() + ", ";
+	if (proto.getpassword() != null) tmp += "port" + proto.getpassword() + ", ";
+	if (proto.getpath() != null) tmp += "port" + proto.getpath() + ", ";
 
 	return tmp + " }";
     }
