0501bb4759a0c15272db273db186560eba85d300
gilles <gilles@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2010-02-10 14:18:47 +0000

    pretty print Protocol
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@398 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/serv/dr/ProtocolUtil.java
+++ src/xtremweb/serv/dr/ProtocolUtil.java
@@ -1,29 +1,24 @@
 package xtremweb.serv.dr;
 
-/*
-<Object name="Protocol">
-<Attribute  name="uid" type="String" desc="Uniq Identifier"/>
-<Attribute  name="name" type="String" desc="Name of the protocol"/>
-<Attribute  name="server" type="String" desc="Server name"/>
-<Attribute  name="port" type="int" desc="Port to contact"/>
-<Attribute  name="login" type="String" desc="Login name (if necessary)"/>
-<Attribute  name="password" type="String" desc="Password (if necessary)"/>
-<Attribute  name="path" type="String" desc="Path (if necessary)"/>
-</Object>
-*/
-
 import xtremweb.core.obj.dr.Protocol;
 
 public class ProtocolUtil {
 
     public static String toString(Protocol proto) {
 	String tmp =  "proto " + proto.getname() + " [" + proto.getuid() + "] = { ";
-	if (proto.getserver() != null) tmp += "server" + proto.getserver() + ", " ;
-	if (proto.getport() != 0) tmp += "port" + proto.getport() + ", ";
-	if (proto.getlogin() != null) tmp += "port" + proto.getlogin() + ", ";
-	if (proto.getpassword() != null) tmp += "port" + proto.getpassword() + ", ";
-	if (proto.getpath() != null) tmp += "port" + proto.getpath() + ", ";
+	if (proto.getname() .equals ("dummy"))
+	    return tmp + " }";
+	else
+	    tmp += proto.getname()+ ":/";
 
+	if (proto.getlogin() != null) { 
+	    tmp += proto.getlogin() ;
+	    if (proto.getpassword() != null) tmp+= "[" + proto.getpassword() + "]";
+	    tmp += "@";
+	}
+	if (proto.getserver() != null) tmp+= proto.getserver();
+	if (proto.getport() != -1) tmp+= ":" + proto.getport();
+	if (proto.getpath() != null) tmp+= "/"+proto.getpath();
 	return tmp + " }";
     }
 
