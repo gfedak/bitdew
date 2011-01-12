62ab816afea701f465e64c9a034ad818b43b94d1
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-01-12 18:48:35 +0000

    test finished, there is a problem when both (FTPAvailable and putget) are launched concurrently
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@511 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/roles/examples/test/FTPAvailableTest.java
+++ src/xtremweb/roles/examples/test/FTPAvailableTest.java
@@ -2,24 +2,28 @@ package xtremweb.roles.examples.test;
 
 import java.io.File;
 import java.util.Properties;
-
+import org.apache.commons.net.io.CopyStreamException;
 import junit.framework.TestCase;
 
 import org.junit.*;
 
+import xtremweb.api.bitdew.BitDew;
 import xtremweb.api.bitdew.BitDewException;
+import xtremweb.api.transman.TransferManager;
 import xtremweb.api.transman.TransferManagerException;
+import xtremweb.core.com.idl.ComWorld;
 import xtremweb.core.obj.dc.Data;
 import xtremweb.role.examples.FTPAvailable;
+import xtremweb.role.examples.PutGet;
 
 public class FTPAvailableTest extends TestCase {
 
     private FTPAvailable ftp;
 
-    private final String testhost = "ftp.lip6.fr";
-    private final String testlogin = "anonymous";
-    private final String testpath = "/pub/linux/distributions/slackware/slackware-current";
-    private final int testport = 21;
+    private final String testhost = "perso.ens-lyon.fr";
+    private final String testlogin = "jsaray";
+    private final String testpath = "/testing";
+    
 
     /**
      * @param args
@@ -32,74 +36,113 @@ public class FTPAvailableTest extends TestCase {
     // TODO look for allowing username and password, currently just anonymous
     // connection
     public void setUp() {
-	ftp = new FTPAvailable(testhost, 21, testlogin, null, testpath);
-	ftp.connect();
+	
+	ftp = new FTPAvailable(testhost, 21, testlogin, "mejvac07", testpath);
+	//ftp.setDebugMode();
+	try {
+	    ftp.connect();
+	    ftp.login();
+	} catch (Exception e) {
+	    fail();
+	    e.printStackTrace();
+	}
     }
 
-    /*
-     * @Test public void testChangeDirectory() { ftp.changeDirectory(testpath);
-     * int response = ftp.getReplyCode()/100; assertEquals(response,2);
-     * assertEquals(ftp.getCurrentDirectory(),testpath); }
-     * 
-     * 
-     * 
-     * @Test public void testConnect() { ftp.connect();
-     * System.out.println("code " + ftp.getReplyCode()); int response =
-     * ftp.getReplyCode()/100; assertEquals(response,2);//first digit in ftp
-     * connection equals 2 means command succesfully issued
-     * //assertEquals(ftp.getCurrentDirectory(),testpath); }
-     */
+    @Test
+    public void testChangeDirectory() {
 
-    
-      @Test public void testGetSignatures() { ftp.changeDirectory(testpath);
-      Properties p = ftp.getSignatures(testpath); assertNotNull(p);
-      assertNotNull(p.get("./COPYRIGHT.TXT"));
-      assertNotNull(p.get("./README_LVM.TXT"));
-      assertNotNull(p.get("./README_CRYPT.TXT"));
-      assertNotNull(p.get("./SPEAK_INSTALL.TXT"));
-      
-      assertEquals(p.get("./COPYRIGHT.TXT"),"4a79c816389ed23235c36244f4210c81");
-      assertEquals(p.get("./README_LVM.TXT"),"1e5ebcf8675508efc07cc6b869dbe6e6");
-      assertEquals(p.get("./README_CRYPT.TXT"),"f7a6a57330634d5eb77eae3f3d5c32bc");
-      assertEquals(p.get("./SPEAK_INSTALL.TXT"),"058f7b10e84d0d51d7725e923ed11cc9");
-
-      }
-     
+	try {
+	    ftp.changeDirectory();
+	    assertEquals(ftp.getCurrentDirectory(), testpath);
+	} catch (Exception e) {
+	    fail();
+	    e.printStackTrace();
+	}
+    }
 
     @Test
-   /* public void testMakeAvailable() {
-	ftp.changeDirectory(testpath);
-	// ftp.getSignatures(testpath);
-	ftp.makeAvailable();
-	 try {
-	// retreive the data object
-	for (int i = 0; i < ftp.getDuids().size(); i++) {
-	    String s = ftp.getData(i);
-	    File file = new File("result"+i+".txt");
-	    Data data;
-	   data = ftp.getBitDewApi().searchDataByUid(s);
-	   ftp.getBitDewApi().get(data, file);
-	    ftp.getTf().waitFor(data);
-	    ftp.getTf().stop();
-	   // log.info("data has been successfully copied to " + fileName);
-	}
-	} catch (BitDewException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    } catch (TransferManagerException e) {
+    public void testGetSignatures() {
+	Properties p;
+	try {
+	    ftp.changeDirectory();
+	    p = ftp.getSignatures();
+	    assertNotNull(p);
+	    /*assertNotNull(p.get("COPYRIGHT.TXT"));
+	    assertNotNull(p.get("README_LVM.TXT"));
+	    assertNotNull(p.get("README_CRYPT.TXT"));
+	    assertNotNull(p.get("SPEAK_INSTALL.TXT"));
+	    assertEquals(p.get("COPYRIGHT.TXT"),
+		    "4a79c816389ed23235c36244f4210c81");
+	    assertEquals(p.get("README_LVM.TXT"),
+		    "1e5ebcf8675508efc07cc6b869dbe6e6");
+	    assertEquals(p.get("README_CRYPT.TXT"),
+		    "f7a6a57330634d5eb77eae3f3d5c32bc");
+	    assertEquals(p.get("SPEAK_INSTALL.TXT"),
+		    "058f7b10e84d0d51d7725e923ed11cc9");*/
+	    assertNotNull(p.get("test.txt"));
+	} catch (Exception e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
-	}*/
+	}
 
-	/*
-	 * Data data = ftp.getBitDewApi().searchDataByUid(dataUid);
-	 * ftp.getBitDewApi().get(data, file); transferManager.waitFor(data);
-	 * transferManager.stop(); assertNotNull
-	 */
-   // }
+    }
+
+    @Test
+    public void testMakeAvailable() {
+	
+	PutGet pg=null;
+	try {
+	     pg = new PutGet("localhost",4325);
+	} catch (Exception e1) {
+	    // TODO Auto-generated catch block
+	    e1.printStackTrace();
+	}
+	 try {
+	     ftp.changeDirectory();
+	     ftp.makeAvailable();
+	for (int i = 0; i < 1; i++) 
+	{
+	    String s = ftp.getData(i);
+	    System.out.println("Trying  " +s);
+	    BitDew bd = new BitDew(ComWorld.getMultipleComms("localhost","rmi",4325,"dr","dt","dc"));
+	    Data d = bd.searchDataByUid(s);
+	    assertNotNull(d);
+	    //pg.get("newfile"+i+".txt",s);
+	   // file = new File("newfile"+i+".txt");
+	    //assertTrue(file.length() > 0);
+	    //log.info("data has been successfully copied to " + fileName);
+	    
+	  /*  File file = new File(fileName);
+
+		//retreive the data object
+		Data data = bitdew.searchDataByUid(dataUid);	    
+		//copy the remote data into the local file
+		bitdew.get(data, file);	
+		//wait for the data transfer to complete and stop the transfer manager
+		transferManager.waitFor(data);
+		transferManager.stop();
+		log.info("data has been successfully copied to " + fileName);*/
+	    
+	    
+	}
+	} 
+	 catch (BitDewException e) {
+		
+		e.printStackTrace();
+		fail();
+	    } catch (TransferManagerException e) {
+		
+	    e.printStackTrace();
+	    fail();
+	} catch (Exception e) {
+	   
+		e.printStackTrace();
+		 fail();
+	    }
+    }
 
     public void tearDown() {
-	ftp.disconnect();
+	//ftp.disconnect();
     }
 
 }
