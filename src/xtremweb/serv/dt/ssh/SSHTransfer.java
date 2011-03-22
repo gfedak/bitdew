c10880eac1718c8088a4040056703ac0c2f8304e
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-03-22 13:58:56 +0000

    comments added
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@651 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/serv/dt/ssh/SSHTransfer.java
+++ src/xtremweb/serv/dt/ssh/SSHTransfer.java
@@ -4,46 +4,138 @@ import xtremweb.core.obj.dc.Data;
 import xtremweb.core.obj.dc.Locator;
 import xtremweb.core.obj.dr.Protocol;
 import xtremweb.core.obj.dt.Transfer;
-import xtremweb.serv.dt.BlockingOOBTransfer;
 import xtremweb.serv.dt.BlockingOOBTransferImpl;
 
-public abstract class SSHTransfer extends BlockingOOBTransferImpl{
+/**
+ * This class represents a ssh transfer, is the base for specialization classes
+ * like ScpTransfer or SftpTransfer
+ * 
+ * @author jsaray
+ * 
+ */
+public abstract class SSHTransfer extends BlockingOOBTransferImpl {
 
-	
-	protected String publicKeyPath;
-	protected String privateKeyPath;
-	protected String passphrase;
-	protected String knownHosts;
-	
-	public SSHTransfer(Data d, Transfer t, Locator rl, Locator ll, Protocol rp,
-			Protocol lp) {
-		super(d, t, rl, ll, rp, lp);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public String getPublicKeyPath() {
-		return publicKeyPath;
-	}
-	public void setPublicKeyPath(String publicKeyPath) {
-		this.publicKeyPath = publicKeyPath;
-	}
-	public String getPrivateKeyPath() {
-		return privateKeyPath;
-	}
-	public void setPrivateKeyPath(String privateKeyPath) {
-		this.privateKeyPath = privateKeyPath;
-	}
-	public String getPasshphrase() {
-		return passphrase;
-	}
-	public void setPasshphrase(String passhphrase) {
-		this.passphrase = passhphrase;
-	}
-	public String getKnownHosts() {
-		return knownHosts;
-	}
-	public void setKnownHosts(String knownHosts) {
-		this.knownHosts = knownHosts;
-	}
-	
+    /**
+     * Path to the public key ex. /home/user/.ssh/id_rsa.pub
+     */
+    protected String publicKeyPath;
+
+    /**
+     * Path to a private key ex. /home/user/.ssh/id_rsa
+     */
+    protected String privateKeyPath;
+
+    /**
+     * Passphrase of the private key (if any private key)
+     */
+    protected String passphrase;
+
+    /**
+     * Known hosts file
+     */
+    protected String knownHosts;
+
+    /**
+     * ssh connection user name
+     */
+    protected String userName;
+
+    /**
+     * ssh connection password
+     */
+    protected String password;
+
+    /**
+     * SSH transfer constructor
+     * 
+     * @param d
+     *            the Data
+     * @param t
+     *            the transfer
+     * @param rl
+     *            the remote locator
+     * @param ll
+     *            the local locator
+     * @param rp
+     *            the remote protocol
+     * @param lp
+     *            the local protocol
+     */
+    public SSHTransfer(Data d, Transfer t, Locator rl, Locator ll, Protocol rp,
+	    Protocol lp) {
+	super(d, t, rl, ll, rp, lp);
+    }
+
+    /**
+     * gets the public key path
+     * 
+     * @return the public key path
+     */
+    public String getPublicKeyPath() {
+	return publicKeyPath;
+    }
+
+    /**
+     * set a public key path
+     * 
+     * @param publicKeyPath
+     */
+    public void setPublicKeyPath(String publicKeyPath) {
+	this.publicKeyPath = publicKeyPath;
+    }
+
+    /**
+     * gets a private key path
+     * 
+     * @return the private key path
+     */
+    public String getPrivateKeyPath() {
+	return privateKeyPath;
+    }
+
+    /**
+     * sets a private key path
+     * 
+     * @param privateKeyPath
+     */
+    public void setPrivateKeyPath(String privateKeyPath) {
+	this.privateKeyPath = privateKeyPath;
+    }
+
+    /**
+     * gets a passphrase
+     * 
+     * @return the passphrase
+     */
+    public String getPasshphrase() {
+	return passphrase;
+    }
+
+    /**
+     * sets a passphrase
+     * 
+     * @param passhphrase
+     */
+    public void setPasshphrase(String passhphrase) {
+	this.passphrase = passhphrase;
+    }
+
+    /**
+     * gets the known host file
+     * 
+     * @return the known hosts file
+     */
+    public String getKnownHosts() {
+	return knownHosts;
+    }
+
+    /**
+     * sets the known hosts file
+     * 
+     * @param knownHosts
+     *            the known hosts file
+     */
+    public void setKnownHosts(String knownHosts) {
+	this.knownHosts = knownHosts;
+    }
 }
