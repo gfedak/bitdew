c61aa212168c5420cbdc9b62d948e804aa10e449
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-03-23 12:55:01 +0000

    test case for dn
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@671 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/serv/dn/test/DnTest.java
@@ -0,0 +1,76 @@
+package xtremweb.serv.dn.test;
+
+import java.rmi.RemoteException;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+import xtremweb.core.com.idl.ComWorld;
+import xtremweb.core.com.idl.ModuleLoaderException;
+import xtremweb.core.iface.InterfaceRMIdn;
+import xtremweb.core.serv.ServiceLoader;
+
+public class DnTest extends TestCase {
+
+    public void setUp() {
+	String[] str = { "dn", "dt", "ds", "dc", "dr" };
+	ServiceLoader s = new ServiceLoader("RMI", 4325, str);
+    }
+
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testmain() {
+	InterfaceRMIdn dn;
+	try {
+	    dn = (InterfaceRMIdn) ComWorld.getComm("localhost", "rmi", 4325,
+		    "dn");
+	    dn.registerService("dt", "lip.ens-lyon.fr");
+	    dn.registerService("ds", "lipgerland.ens-lyon.fr");
+	    dn.registerService("clacla", "plapla");
+	    String host = dn.getServiceAddress("dt");
+	    System.out.println(" The host for dt is " + host);
+
+	    host = dn.getServiceAddress("ds");
+	    System.out.println("The host for ds is " + host);
+
+	    host = dn.getServiceAddress("clacla");
+	    System.out.println("The host for ds is " + host);
+
+	    host = dn.getServiceAddress("claclap");
+	    System.out.println("The host for ds is " + host);
+
+	} catch (ModuleLoaderException e) {
+	    fail();
+	    e.printStackTrace();
+	} catch (RemoteException e) {
+	    fail();
+	    e.printStackTrace();
+	}
+    }
+    /*
+     * public void setUp() { } public void tearDown() {
+     * 
+     * }
+     * 
+     * public void testdn() { String[] servs = {"dn","dt","dc","dr","ds"};
+     * ServiceLoader sl = new ServiceLoader(); InterfaceRMIdn dn; try { dn =
+     * (InterfaceRMIdn)ComWorld.getComm("localhost", "rmi", 4325, "dn");
+     * dn.registerService("dt","lip.ens-lyon.fr"); dn.registerService("ds",
+     * "lipgerland.ens-lyon.fr");
+     * 
+     * String host = dn.getServiceAddress("dt");
+     * System.out.println(" The host for dt is " + host);
+     * 
+     * host = dn.getServiceAddress("ds");
+     * System.out.println("The host for ds is " + host);
+     * 
+     * } catch (ModuleLoaderException e) { // TODO Auto-generated catch block
+     * e.printStackTrace(); } catch (RemoteException e) { // TODO Auto-generated
+     * catch block e.printStackTrace(); } }
+     */
+
+}
