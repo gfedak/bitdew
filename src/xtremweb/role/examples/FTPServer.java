434f009edf3d869d55b25343a4a81467d73d6c98
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-05-23 19:41:39 +0000

    changes to comply with json and blocking transfer modifications
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@758 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/role/examples/FTPServer.java
+++ src/xtremweb/role/examples/FTPServer.java
@@ -35,36 +35,36 @@ public class FTPServer {
      * dt, ds) and makes existing files on a remote ftp folder available in
      * bitdew. Then you can use the "CommandLineTool get" option to download the
      * files served by the FTP server. <ol> <li> Download and locate the file <a
-     * href="http://www.bitdew.net/xtremweb.properties">xtremweb.properties</a> in the same directory
+     * href="http://www.bitdew.net/properties.json">properties.json</a> in the same directory
      * than the jar distribution.</li>
      * 
-     * <li> Uncomment the lines concerning the ftp configuration, by
-     * default it will connects to ftp.lip6.fr, but you can change the
-     * configuration with your own ftp configuration data
-     * (hostname,port,directory to be made available, username and
-     * password).</li>
+     * <li> This json file contains a list of repositories, one by protocol</li>
      * 
-     * @code #xtremweb.serv.dr.protocols=dummy http ftp
-     * #xtremweb.serv.dr.ftp.server=ftp.lip6.fr #xtremweb.serv.dr.ftp.port=21
-     * #xtremweb.serv.dr.ftp.login=anonymous
-     * #xtremweb.serv.dr.ftp.path=/pub/linux/distributions/slackware/slackware-current
-     * #xtremweb.serv.dr.ftp.passwd=anonymous
+     * @code 
+     * {	xtremweb.role : {ui: "true"},
+	xtremweb.core.db : {driver: "org.hsqldb.jdbcDriver", url: "jdbc:hsqldb:mem:test",user:"sa",password: "", connectionPooling: "DBCP"},
+	xtremweb.serv.dr.ftp : {name:"ftp",server:"ftp.lip6.fr",port:21,login:"anonymous", passwd:"anonymous",path:"/pub/linux/distributions/slackware/slackware-current"},
+	xtremweb.core.com.rmi: {port: 4325},
+	xtremweb.serv.dr : {protocols: "dummy http ftp"},
+	xtremweb.serv.dr.ftp : {server: "ftp.lip6.fr", login: "anonymous", path:"/pub/linux/distributions/slackware/slackware-current", passwd:"anonymous"},
+	xtremweb.core.http: {port: 8080,path:"/data",documentRoot:".",uploadServlet:"/fileupload"},
+	xtremweb.serv.dr.http: {name:"http",port:8080,path:"data"},
+	xtremweb.serv.dr.dummy: {name: "dummy"},
+	xtremweb.serv.dr.bittorrent: {name:"bittorrent",path:"torrents",port:6969},
+	xtremweb.serv.dt: {protocols: "dummy http"},
+	xtremweb.serv.dt.bittorrent: {makeTorrentExe:"/Users/fedak/shared/projects/bitdew/python/btmakemetafile.py",azureusjar:"/Users/fedak/shared/projects/bitdew/lib/azureus.jar",
+								  trackerExec:"/Users/fedak/shared/projects/bitdew/python/bttrack.py",
+								  trackerOption:"--dfile dstate"},
+	xtremweb.core.handler: {perf: "true"}							  
+}
+
      * 
-     * @endcode <li> The file CHECKSUMS.md5 must exist in the remote ftp folder.
-     * FTPServer will use the checksum information provided in this file in
-     * order to successfully download the file. CHECKSUMS.md5 should have the
-     * following format <md5_checksum> './'<filename>, for example</li>
+     * @endcode 
      * 
-     * @code 928143c5d1a7825bc5f3c1d18fde1e31 ./ANNOUNCE.13_1
-     * c08333e6569a6fd66fd59999bfa94eb5 ./BOOTING.TXT
-     * 19494cd00c011661a882c23772eaa7f0 ./CHANGES_AND_HINTS.TXT
-     * 18810669f13b87348459e611d31ab760 ./COPYING
-     * 
-     * @endcode
      * 
      * <li> To run the example, execute the following command : </li>
      * 
-     * @code java -cp bitdew-stand-alone-X.X.X.jar xtremweb.role.examples.FTPServer xtremweb.properties
+     * @code java -cp bitdew-stand-alone-X.X.X.jar xtremweb.role.examples.FTPServer properties.json
      * 
      * @endcode
      * 
@@ -84,7 +84,7 @@ public class FTPServer {
      * @endcode <li> You can execute CommandLineTool get command in order to
      * retrieve a file from the ftp server, for example :</li>
      * 
-     * @code java -jar bitdew-stand-alone-X.X.X.jar get 639edd60-1cbc-31e0-ab7e-c0c64755d3e6 LOCAL_BOOTING.TXT
+     * @code java -jar bitdew-stand-alone-X.X.X.jar get 639edd60-1cbc-31e0-ab7e-c0c64755d3e6 ftp LOCAL_BOOTING.TXT
      * 
      * @endcode
      * 
@@ -188,7 +188,7 @@ public class FTPServer {
 	// get integrity signature for each file, this is needed so the transfer
 	// manager
 	// can successfuly complete a transfer
-	HashMap digests = getMd5Signatures();
+	
 	cl.enterLocalPassiveMode();
 	FTPFile[] files = cl.listFiles();
 	log.info("calculating md5 signatures from CHECKSUMS.md5");
@@ -200,8 +200,8 @@ public class FTPServer {
 	    if(name.equals("CHECKSUMS.md5") || name.equals("CHECKSUMS.md5.asc"))//checksum has not a md5 in itself, so we skip it
 		continue;
 	    // create a bitdew data setting the ftp protocol
-	    Data data = bd.createData(name, "FTP", files[i].getSize(),
-		    (String) digests.get(name));
+	    Data data = bd.createData(name);
+	    data.setoob("ftp");
 	    // creates a remote locator for this data
 	    Locator remote_locator = bd.createRemoteLocator(data, "ftp");
 	    // gives data and locator to bitdew so it can associate them
@@ -220,51 +220,6 @@ public class FTPServer {
      */
     public void disconnect() throws IOException {
 	cl.disconnect();
-    }
-
-    /**
-     * Assuming there is a SIGNATURES.md5 file on pathname, this method parses
-     * that file and builds a properties structure to provide an easy access to
-     * the md5 file signatures.
-     * 
-     * @return a hash structure <name of file,md5 signature>
-     * @exception Exception
-     *                if an error occurs
-     */
-    private HashMap getMd5Signatures() throws Exception {
-	HashMap p = new HashMap();
-	boolean exist = false;
-	FileOutputStream fos;
-	fos = new FileOutputStream(new File("sigs.md5"));
-	boolean result;
-	cl.enterLocalPassiveMode();
-	// retrieve the file CHECKSUMS.md5 from the remote directory, this file
-	// must
-	// exist to guarantee the correct example execution
-	result = cl.retrieveFile("CHECKSUMS.md5", fos);
-	fos.close();
-	if (!result)
-	    throw new FileNotFoundException(
-		    "There is no CHECKSUMS.md5 file on that directory");
-	BufferedReader br = new BufferedReader(new FileReader("sigs.md5"));
-	String str = br.readLine();
-	// parse each line by splitting by ./ character
-	while (str != null) {
-	    if (str.split("/").length == 2) {
-		exist = true;
-		log.debug(str + " is a file");
-		String[] tokens = str.split("  ./");
-		if (tokens.length != 2)
-		    throw new Exception(
-			    "The md5 file is not propertly parsed I'm waiting <md5> ./<filename>");
-		p.put(tokens[1], tokens[0]);
-	    }
-	    str = br.readLine();
-	}
-	if (!exist)
-	    throw new Exception(
-		    "The md5 file is not propertly parsed I'm waiting <md5> ./<filename>");
-	return p;
     }
 
     /**
