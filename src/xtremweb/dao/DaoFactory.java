ff410ee3eaa4a75016e58a07bbf2beb774fd7b83
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-07-19 14:51:35 +0000

    data import added
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@784 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/dao/DaoFactory.java
+++ src/xtremweb/dao/DaoFactory.java
@@ -1,11 +1,29 @@
 package xtremweb.dao;
 
+import xtremweb.dao.data.*;
+
 public class DaoFactory {
 
     public static InterfaceDao getInstance(String clazz)
-	    throws InstantiationException, IllegalAccessException,
-	    ClassNotFoundException {
-	return (InterfaceDao) Class.forName(clazz).newInstance();
+    {
+	
+	    
+	    try {
+		return (InterfaceDao) Class.forName(clazz).newInstance();
+	    } catch (InstantiationException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    } catch (IllegalAccessException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    } catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	return null;
+	    
+	
+	
     }
 
 }
