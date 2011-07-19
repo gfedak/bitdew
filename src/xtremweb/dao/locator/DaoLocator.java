50570f24699168afcd7f1388c13fae1036de2b02
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-07-19 14:58:31 +0000

    comply with naming standards
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@786 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/dao/locator/DaoLocator.java
@@ -0,0 +1,12 @@
+package xtremweb.dao.locator;
+
+import xtremweb.dao.DaoJDOImpl;
+
+/**
+ * This class interface with locator table thorugh jdo
+ * @author jsaray
+ *
+ */
+public class DaoLocator extends DaoJDOImpl {
+
+}
