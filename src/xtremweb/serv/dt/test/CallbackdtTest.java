a984ef27efd25998349ab63e1f12c2766e2ee600
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-07-20 14:18:37 +0000

    Test succeeded
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@793 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/serv/dt/test/CallbackdtTest.java
@@ -0,0 +1,74 @@
+package xtremweb.serv.dt.test;
+
+import java.rmi.RemoteException;
+
+import org.junit.Test;
+
+import xtremweb.api.transman.TransferStatus;
+import xtremweb.core.obj.dt.Transfer;
+import xtremweb.serv.dt.Callbackdt;
+import junit.framework.TestCase;
+
+public class CallbackdtTest extends TestCase {
+    
+    
+    
+    
+    public void setUp()
+    {
+	
+    }
+    
+   @Test 
+    public void testClass()
+    {
+	Callbackdt dt;
+	
+	dt = new Callbackdt();
+	Transfer t1 = new Transfer();
+	t1.setstatus(TransferStatus.PENDING);
+	
+	Transfer t2 = new Transfer();
+	t2.setstatus(TransferStatus.PENDING);
+	
+	Transfer t3 = new Transfer();
+	t3.setstatus(TransferStatus.PENDING);
+	
+	String uid1;
+	try {
+	    uid1 = dt.putTransfer(t1);
+	    String uid2 = dt.putTransfer(t2);
+	    String uid3 = dt.putTransfer(t3);
+	
+	    boolean b1 = dt.poolTransfer(uid1);
+	    assertFalse(b1);
+	    boolean b2 = dt.poolTransfer(uid2);
+	    assertFalse(b2);
+	    boolean b3 = dt.poolTransfer(uid3);
+	    assertFalse(b3);
+	
+	    dt.setTransferStatus(uid1,TransferStatus.COMPLETE);
+	    dt.setTransferStatus(uid2, TransferStatus.COMPLETE);
+	    dt.setTransferStatus(uid3, TransferStatus.COMPLETE);
+	    
+	    boolean b4 = dt.poolTransfer(uid1);
+	    assertTrue(b4);
+	    boolean b5 = dt.poolTransfer(uid2);
+	    assertTrue(b5);
+	    boolean b6 = dt.poolTransfer(uid3);
+	    assertTrue(b6);
+	    
+	    
+	
+	} catch (RemoteException e) {
+	    e.printStackTrace();
+	}
+    }
+    
+    @Test
+    public void testRMI()
+    {
+	
+    }
+
+}
