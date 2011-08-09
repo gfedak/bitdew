fdb2e8a6ea16fdf62af160dc3a529578dad21ec4
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-08-09 11:16:55 +0000

    dplt added
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@840 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/serv/dc/ddc/dplt/DPLTDistributedDataCatalog.java
@@ -0,0 +1,58 @@
+package xtremweb.serv.dc.ddc.dplt;
+
+import fr.inria.graal.sbam.core.api.IPeer;
+import fr.inria.graal.sbam.core.impl.FactoryPeer;
+import fr.inria.graal.sbam.core.services.PfConfigService;
+import fr.inria.graal.sbam.dlpt.api.IClient;
+import fr.inria.graal.sbam.dlpt.impl.DlptClient;
+import xtremweb.serv.dc.ddc.DDCException;
+import xtremweb.serv.dc.ddc.DistributedDataCatalog;
+import xtremweb.serv.dc.ddc.DistributedDataCatalogImpl;
+import java.util.List;
+
+
+public class DPLTDistributedDataCatalog extends DistributedDataCatalogImpl implements DistributedDataCatalog {
+
+	private IClient client;
+	private PfConfigService pconfig;
+
+	public void start() throws DDCException {
+		
+		scalexp.BootstrapExp.observation = true;
+		scalexp.BootstrapExp.bootstrapExp(new String[]{"DEBUG"});
+		pconfig = PfConfigService.getInstance();
+
+	}
+
+
+	public String entryPoint() throws DDCException {
+		
+		//TODO Convert the object pconfig to String
+		
+		
+		return pconfig.toString();
+	}
+
+
+	public void join(String url) throws DDCException {
+		
+		PfConfigService.getInstance().loadConfig(pconfig);
+		IPeer locPeer = FactoryPeer.createLocalPeer(new FactoryPeer.PEERGROUP[]{FactoryPeer.PEERGROUP.DLPTCLIENTHOSTER});
+		client = new DlptClient(locPeer);
+	}
+	
+	/**
+	 * Value must be serializable !!!!!
+	 * XSTREAM serialize objects to XML
+	 */
+	public void publish(String key, String value) throws DDCException {
+		client.putResourcesSeq(value, key);
+	}
+
+
+	public List search(String key) throws DDCException {
+		//TODO change originally string by a List or Collection
+		return client.getResources(key);
+	}
+
+}
