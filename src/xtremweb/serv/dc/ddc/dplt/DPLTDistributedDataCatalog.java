057726174dcd0e4348c7e346de8a6d600ae5da15
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-08-22 11:53:39 +0000

    changes to dlpt
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@861 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/serv/dc/ddc/dplt/DPLTDistributedDataCatalog.java
+++ src/xtremweb/serv/dc/ddc/dplt/DPLTDistributedDataCatalog.java
@@ -1,5 +1,8 @@
 package xtremweb.serv.dc.ddc.dplt;
 
+
+
+import com.google.gson.Gson;
 import fr.inria.graal.sbam.core.api.IPeer;
 import fr.inria.graal.sbam.core.impl.FactoryPeer;
 import fr.inria.graal.sbam.core.services.PfConfigService;
@@ -8,14 +11,30 @@ import fr.inria.graal.sbam.dlpt.impl.DlptClient;
 import xtremweb.serv.dc.ddc.DDCException;
 import xtremweb.serv.dc.ddc.DistributedDataCatalog;
 import xtremweb.serv.dc.ddc.DistributedDataCatalogImpl;
+
+import java.io.Serializable;
 import java.util.List;
 
-
+/**
+ * This class implements a DHT using the DLPT prefix tree
+ * @author josefrancisco
+ *
+ */
 public class DPLTDistributedDataCatalog extends DistributedDataCatalogImpl implements DistributedDataCatalog {
-
+	
+	/**
+	 * DLPT Client
+	 */
 	private IClient client;
+	
+	/**
+	 * This object contains server initialization
+	 */
 	private PfConfigService pconfig;
-
+	
+	/**
+	 * Start the DLPT locally
+	 */
 	public void start() throws DDCException {
 		
 		scalexp.BootstrapExp.observation = false;
@@ -24,19 +43,25 @@ public class DPLTDistributedDataCatalog extends DistributedDataCatalogImpl imple
 
 	}
 
-
+	/**
+	 * Get the entry point to this client
+	 */
 	public String entryPoint() throws DDCException {
 		
 		//TODO Convert the object pconfig to String
+		Gson gson = new Gson();
+		String json = gson.toJson(pconfig);
 		
-		
-		return pconfig.toString();
+		return json;
 	}
 
-
+	/**
+	 * Join to this DLPT
+	 */
 	public void join(String url) throws DDCException {
-		
-		PfConfigService.getInstance().loadConfig(pconfig);
+		Gson gson = new Gson();
+		PfConfigService newconfig = gson.fromJson(url,PfConfigService.class);
+		PfConfigService.getInstance().loadConfig(newconfig);
 		IPeer locPeer = FactoryPeer.createLocalPeer(new FactoryPeer.PEERGROUP[]{FactoryPeer.PEERGROUP.DLPTCLIENTHOSTER});
 		client = new DlptClient(locPeer);
 	}
@@ -45,11 +70,14 @@ public class DPLTDistributedDataCatalog extends DistributedDataCatalogImpl imple
 	 * Value must be serializable !!!!!
 	 * XSTREAM serialize objects to XML
 	 */
-	public void publish(String key, String value) throws DDCException {
+	public void publish(String key, Serializable value) throws DDCException {
 		client.putResourcesSeq(value, key);
 	}
 
-
+	/**
+	 * Search for a specified key on the DLPT
+	 * @return list of objects matching the key
+	 */
 	public List search(String key) throws DDCException {
 		//TODO change originally string by a List or Collection
 		return client.getResources(key);
