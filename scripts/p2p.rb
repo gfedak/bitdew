c8ae06a31728fe06686f743cfd07a8da5337e6fe
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-08-22 11:46:21 +0000

    some improvements to p2p script
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@860 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- scripts/p2p.rb
+++ scripts/p2p.rb
@@ -1,9 +1,30 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'net/ssh'
-#%x(uniq $OAR_NODEFILE > nodelist)
 
+
+#allhosts will store the Grid500 OAR_NODEFILE content 
+iout = IO.popen("uniq $OAR_NODEFILE")
+allhosts = iout.readlines
+#the folder that contains all songs
 path = ARGV[0]
+#number of peers different from the bootstrap node from OAR_NODEFILE, effectively making part of the experiment, offset <= NUMBER_OF_LINES(OAR_NODEFILE)
+offset = ARGV[1].to_i
+#how many files will be send to a host
+filesperhost = ARGV[2].to_i
+
+File.open('/home/jsaray/nodelist','w') do |f|
+  allhosts[0..offset].each{|line|
+     f.write(line)
+}
+end
+
+if File.exists? "/home/jsaray/nodelist" then
+else
+ raise "The file nodelist was not correctly created, please try again"
+end
+
+puts "allhosts name is " + allhosts.class.name  
 nodes = IO.readlines("/home/jsaray/nodelist")
 centralnode = nodes[0] 
 term = nodes[1]
@@ -15,10 +36,11 @@ output = %x(taktuk -d-1 -f nodelist broadcast exec { 'rm -rf *'  })
 
 mkdir = %x(taktuk -d-1 -f nodelist broadcast exec { mkdir songs })
 puts mkdir
+#the executable code is sent to different peers
 %x(taktuk -d-1 -f nodelist broadcast put { /home/jsaray/bitdewp2pn.jar } { /home/jsaray/bitdewp2pn.jar })
 %x(taktuk -d-1 -f nodelist broadcast put { /home/jsaray/bitdew-stand-alone-0.2.6.jar } { /home/jsaray/bitdew-stand-alone-0.2.6.jar })
 centralnode = centralnode.slice(/[^\n]*/)
-
+#services are started on central node, special attention is dc, which contains a ddc and the DHT 
 Net::SSH.start(centralnode,"jsaray") do |ssh|
   ssh.exec "java -cp bitdewp2pn.jar:bitdew-stand-alone-0.2.6.jar xtremweb.role.cmdline.CommandLineTool serv dc dt ds dr > /home/jsaray/servout"+centralnode+" 2> /home/jsaray/serverr"+centralnode+" &"
 end
@@ -30,21 +52,23 @@ puts "allfiles size is " + allfiles.length.to_s
 puts "allfiles sub 5 is " + allfiles.to_s
 
 posi = 2
+#the files on directory are sent in blocks of signaled by variable filesperhost
 arr.each{|line|   
     line = line.slice(/[^\n]*/)
 Net::SSH.start(line,"jsaray")do |ssh|
         ssh.exec "java -cp bitdewp2pn.jar:bitdew-stand-alone-0.2.6.jar xtremweb.role.cmdline.CommandLineTool serv dt dr ds dc > /home/jsaray/repout 2> /home/jsaray/repoerr &"
         end
     sleep(3)
-    allfiles[posi..posi+4].each{|file|        
+    allfiles[posi..posi+filesperhost-1].each{|file|        
         puts "sending file " + file +" to host " + line
 	IO.popen("scp \"" +path+"/"+ file +  "\" " + line+":songs/")do|f|
             f.readlines
         end
     }
-  posi = posi + 5
+  posi = posi + filesperhost 
   puts "posini is " + posi.to_s + " posfi is " + posi.to_s
 }
+#The program P2PSongs is executed in all peers
 arr = nodes[1,(nodes.length-1)]
      arr.each{|line|
          line = line.slice(/[^\n]*/)
