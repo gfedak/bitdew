e8246f5598053b4ce633c708c7cb5e2e645326ab
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-10-07 17:31:01 +0000

    class to get the number of ips having a md5
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@895 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/core/http/GetIpServlet.java
@@ -0,0 +1,104 @@
+package xtremweb.core.http;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import xtremweb.api.bitdew.BitDew;
+import xtremweb.api.bitdew.BitDewException;
+import xtremweb.core.com.idl.ComWorld;
+import xtremweb.core.com.idl.ModuleLoaderException;
+import xtremweb.core.iface.InterfaceRMIdc;
+import xtremweb.core.iface.InterfaceRMIdr;
+import xtremweb.core.iface.InterfaceRMIds;
+import xtremweb.core.iface.InterfaceRMIdt;
+import xtremweb.core.log.Logger;
+import xtremweb.core.log.LoggerFactory;
+
+public class GetIpServlet extends HttpServlet {
+
+    /**
+     * Data catalog
+     */
+    private InterfaceRMIdc ddc;
+
+    /**
+     * Data repository
+     */
+    private InterfaceRMIdr dr;
+
+    /**
+     * Data scheduler
+     */
+    private InterfaceRMIds ds;
+
+    /**
+     * Data Transfer
+     */
+    private InterfaceRMIdt dt;
+
+    /**
+     * Bitdew API
+     */
+    private BitDew bd;
+
+    /**
+     * Logger
+     */
+    protected Logger log = LoggerFactory.getLogger("P2PServlet");
+
+    public GetIpServlet() {
+	try {
+
+	    String LOCAL_ADDRESS = InetAddress.getLocalHost().getHostAddress();
+	    ddc = (InterfaceRMIdc) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dc");
+	    dr = (InterfaceRMIdr) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dr");
+	    dt = (InterfaceRMIdt) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dt");
+	    ds = (InterfaceRMIds) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "ds");
+	    bd = new BitDew(ddc, dr, dt, ds);
+	} catch (ModuleLoaderException e) {
+	    e.printStackTrace();
+	} catch (UnknownHostException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	String md5 = request.getParameter("md5");
+	List ips;
+	try {
+	    ips = bd.ddcSearch(md5);
+
+	    String responsexml = "<html><head></head><body><center><h3>Results found for : "
+		    + md5 + "</h3><table border=\"1\"><tr><td>Ip</td></tr>";
+	    for (int i = 0; i < ips.size(); i++) {
+		responsexml += "<tr><td>" + ips.get(i) + "</td></tr>";
+	    }
+	    responsexml += "</table></center></body></html>";
+	    response.getWriter().println(responsexml);
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_OK);
+	} catch (BitDewException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+
+    }
+
+}
