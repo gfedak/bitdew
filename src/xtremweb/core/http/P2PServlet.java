d6f8be376bfc855f57aba8c31d7d88a1196eb455
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-10-15 18:44:29 +0000

    idented and parametrized
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@905 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/core/http/P2PServlet.java
+++ src/xtremweb/core/http/P2PServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +15,8 @@ import xtremweb.api.bitdew.BitDew;
 import xtremweb.api.bitdew.BitDewException;
 import xtremweb.core.com.idl.ComWorld;
 import xtremweb.core.com.idl.ModuleLoaderException;
+import xtremweb.core.conf.ConfigurationException;
+import xtremweb.core.conf.ConfigurationProperties;
 import xtremweb.core.iface.InterfaceRMIdc;
 import xtremweb.core.iface.InterfaceRMIdr;
 import xtremweb.core.iface.InterfaceRMIds;
@@ -23,100 +26,129 @@ import xtremweb.core.log.LoggerFactory;
 import xtremweb.role.examples.obj.SongBitdew;
 
 /**
- * This class builds a HTTP response with the list of songs matching a specific term 
- * entered by the user
+ * This class builds a HTTP response with the list of songs matching a specific
+ * term entered by the user
+ * 
  * @author josefrancisco
- *
+ * 
  */
 public class P2PServlet extends HttpServlet {
-	
-	/**
-	 * Data catalog
-	 */
-	private InterfaceRMIdc ddc;
-	
-	/**
-	 * Data repository
-	 */
-	private InterfaceRMIdr dr;
-	
-	/**
-	 * Data scheduler
-	 */
-	private InterfaceRMIds ds;
-	
-	/**
-	 * Data Transfer
-	 */
-	private InterfaceRMIdt dt;
-	
-	/**
-	 * Bitdew API
-	 */
-	private BitDew bd;
-	
-	/**
-	 * Logger
-	 */
-	protected Logger log = LoggerFactory.getLogger("P2PServlet");
-	
-	/**
-	 * Constructor
-	 */
-	public P2PServlet()
-	{
-		try {
-			
-			String LOCAL_ADDRESS = InetAddress.getLocalHost().getHostAddress();
-			ddc = (InterfaceRMIdc)ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325, "dc");
-			dr = (InterfaceRMIdr)ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325, "dr");
-			dt = (InterfaceRMIdt)ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325, "dt");
-			ds = (InterfaceRMIds)ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325, "ds");
-			bd = new BitDew(ddc,dr,dt,ds);
-		} catch (ModuleLoaderException e) {
-			e.printStackTrace();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+    /**
+     * Data catalog
+     */
+    private InterfaceRMIdc ddc;
+
+    /**
+     * Data repository
+     */
+    private InterfaceRMIdr dr;
+
+    /**
+     * Data scheduler
+     */
+    private InterfaceRMIds ds;
+
+    /**
+     * Data Transfer
+     */
+    private InterfaceRMIdt dt;
+
+    /**
+     * Bitdew API
+     */
+    private BitDew bd;
+
+    /**
+     * Logger
+     */
+    protected Logger log = LoggerFactory.getLogger("P2PServlet");
+
+    private String BOOTSTRAP_NODE;
+    private boolean SSH_TUNNELING;
+
+    /**
+     * Constructor
+     */
+    public P2PServlet() {
+	try {
+
+	    String LOCAL_ADDRESS = InetAddress.getLocalHost().getHostAddress();
+	    ddc = (InterfaceRMIdc) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dc");
+	    dr = (InterfaceRMIdr) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dr");
+	    dt = (InterfaceRMIdt) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dt");
+	    ds = (InterfaceRMIds) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "ds");
+	    bd = new BitDew(ddc, dr, dt, ds);
+	    Properties props = ConfigurationProperties.getProperties();
+	    String ssht = (String)props.getProperty("xtremweb.core.http.sshTunneling");
+
+	   
+	    String bootstrapnode = (String)props.getProperty("xtremweb.core.http.bootstrapNode");
+	    
+	    BOOTSTRAP_NODE =  (bootstrapnode != null) ? bootstrapnode : "localhost" ;
+	    if (ssht!=null || ssht.equals("yes")) {
+		BOOTSTRAP_NODE = "localhost";
+	    }
+		    
+	} catch (ModuleLoaderException e) {
+	    e.printStackTrace();
+	} catch (UnknownHostException e) {
+	    e.printStackTrace();
+	} catch (ConfigurationException e) {
+	    e.printStackTrace();
 	}
-	
-	/**
-	 * Returns a list of songs matching a term given by the user
-	 * @request HTTP request with a user-specified term
-	 * @response The response the servlet will produce
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{	String HOST_NAME = InetAddress.getLocalHost().getHostName();
-		String param = request.getParameter("term");
-		log.debug("parameter term : " + param);
-		response.setContentType("xml");		
-		String responsexml = "<html><head></head><body><center><h3>Results found for : " + param + "</h3><table border=\"1\"><tr><td>Song Name</td><td>MD5</td></tr>";
-		List l;
-		try {
-			l = bd.ddcSearch(param);
-			//for each result write it on a html table
-			for(int i = 0; i < l.size(); i++)
-			{
-				responsexml+="<tr>" +
-						        "<td><a href=http://localhost:8080/download?songname="+((SongBitdew)l.get(i)).getFilename()+"&md5="+((SongBitdew)l.get(i)).getMd5()+">"+((SongBitdew)l.get(i)).getFilename()+"</td>"+
-						        "<td><a href=http://localhost:8080/getips?md5="+((SongBitdew)l.get(i)).getMd5()+">"+((SongBitdew)l.get(i)).getMd5()+"</td>"+
-						     "</tr>";
-			}
-			responsexml+="</table></center></body></html>";
-			response.getWriter().println(responsexml);
-			response.setContentType("text/html");
-			response.setStatus(HttpServletResponse.SC_OK);	
-			} catch (BitDewException e) {
-			e.printStackTrace();
-		}
+    }
+
+    /**
+     * Returns a list of songs matching a term given by the user
+     * 
+     * @request HTTP request with a user-specified term
+     * @response The response the servlet will produce
+     */
+    protected void doGet(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+	String HOST_NAME = InetAddress.getLocalHost().getHostName();
+	String param = request.getParameter("term");
+	log.debug("parameter term : " + param);
+	log.debug("bootstrap node value is " + BOOTSTRAP_NODE);
+	response.setContentType("xml");
+	String responsexml = "<html><head></head><body><center><h3>Results found for : "
+		+ param
+		+ "</h3><table border=\"1\"><tr><td>Song Name</td><td>MD5</td></tr>";
+	List l;
+	try {
+	    l = bd.ddcSearch(param);
+	    // for each result write it on a html table
+	    for (int i = 0; i < l.size(); i++) {
+		responsexml += "<tr>" + "<td><a href=http://" + BOOTSTRAP_NODE
+			+ ":8080/download?songname="
+			+ ((SongBitdew) l.get(i)).getFilename() + "&md5="
+			+ ((SongBitdew) l.get(i)).getMd5() + ">"
+			+ ((SongBitdew) l.get(i)).getFilename() + "</td>"
+			+ "<td><a href=http://" + BOOTSTRAP_NODE
+			+ ":8080/getips?md5="
+			+ ((SongBitdew) l.get(i)).getMd5() + ">"
+			+ ((SongBitdew) l.get(i)).getMd5() + "</td>" + "</tr>";
+	    }
+	    responsexml += "</table></center></body></html>";
+	    response.getWriter().println(responsexml);
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_OK);
+	} catch (BitDewException e) {
+	    e.printStackTrace();
 	}
-	
-	/**
-	 * Http POST method is not used for the moment
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-	}
+    }
+
+    /**
+     * Http POST method is not used for the moment
+     */
+    protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+
+    }
 
 }
