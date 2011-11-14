eecd4c2eb42d380537298057a9d493bac85f2e5e
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-11-14 12:02:54 +0000

    no more bitdewsong
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@927 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/core/http/DownloadSongServlet.java
+++ src/xtremweb/core/http/DownloadSongServlet.java
@@ -1,11 +1,18 @@
 package xtremweb.core.http;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+
+
+import javax.jdo.listener.DirtyLifecycleListener;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +23,7 @@ import xtremweb.core.log.Logger;
 import xtremweb.core.log.LoggerFactory;
 import xtremweb.role.examples.P2PClient;
 
+import org.apache.commons.codec.binary.Hex;
 /**
  * This class builds a HTTP response when a user attempts to download a song
  * 
@@ -38,13 +46,7 @@ public class DownloadSongServlet extends HttpServlet {
 	 * 
 	 */
     public DownloadSongServlet() {
-	try {
-	    String LOCAL_ADDRESS = InetAddress.getLocalHost().getHostAddress();
-	    client = new P2PClient(LOCAL_ADDRESS);
-	} catch (UnknownHostException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
+	client = new P2PClient();
     }
 
     /**
@@ -58,25 +60,50 @@ public class DownloadSongServlet extends HttpServlet {
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-	String md5 = request.getParameter("md5");
-	log.debug("md5 is " + md5);
-	ServletOutputStream op = response.getOutputStream();
-	String songname = request.getParameter("songname");
-	client.download(songname, md5);
-	File file = new File(songname);
-	response.setContentType("application/octet-stream");
-	response.setContentLength((int) file.length());
-	response.setHeader("Content-Disposition", "attachment; filename=\""
-		+ songname + "\"");
-	FileInputStream in = new FileInputStream(songname);
-	int length;
-	while ((in != null) && ((length = in.read()) != -1))
-	    op.write(length);
-	in.close();
-	op.flush();
-	op.close();
+	String directory = request.getParameter("directory");
+	if(request.getParameter("md5") != null && request.getParameter("songname") != null)
+	{
+	    String md5 = request.getParameter("md5");
+	    log.debug("md5 is " + md5);
+	    String songname = request.getParameter("songname");
+	    client.download(songname, md5);
+	    client.republish(songname,md5);
+	}
+	String answer = "<table><tr><td>File name</td><td>MD5</td></tr>";
+	 try {
+	File dir = new File(directory);
+	if(dir.exists())
+	{ 
+        	File[] file = dir.listFiles();
+        	for(int i = 0; i < file.length ; i++ )
+        	{   String item = file[i].getName();
+        	    byte[] b = new byte [(int)file[i].length()];
+        	    answer+="<tr>";
+        	    if (!file[i].isDirectory()){
+        		FileInputStream fis = new FileInputStream(file[i]) ;	    
+        		fis.read(b);
+        		MessageDigest md5dig;
+        		md5dig = MessageDigest.getInstance("MD5");
+        		byte[] bytes = md5dig.digest(b);
+        		String hexw =  new String(Hex.encodeHex(bytes));
+        		answer += "<td>"+item + "</td><td>" + hexw + "</td>";
+        	    }
+        	    answer+="</tr>";
+        	}
+        	answer+= "</textarea>";
+        	response.getWriter().println(answer);
+        	
 	//Seed the file in the P2P network.
-	client.republish(songname,md5);
+	}else{
+	    response.getWriter().println("<p>That directory do not exist in your file system !!</p>");
+	}
+	response.setContentType("text/html");
+	response.setStatus(HttpServletResponse.SC_OK);
+	 } 
+	catch (NoSuchAlgorithmException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }	
     }
 
     /**
