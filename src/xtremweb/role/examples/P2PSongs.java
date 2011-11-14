0361ddf197fec6a1628798bce08a5e4b95bf0c4b
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-11-14 11:58:25 +0000

    no more bitdewsong
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@924 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/role/examples/P2PSongs.java
+++ src/xtremweb/role/examples/P2PSongs.java
@@ -3,11 +3,9 @@ package xtremweb.role.examples;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.List;
 
 import xtremweb.api.bitdew.BitDew;
 import xtremweb.api.bitdew.BitDewException;
-import xtremweb.api.transman.TransferManagerException;
 import xtremweb.core.com.idl.ComWorld;
 import xtremweb.core.com.idl.ModuleLoaderException;
 import xtremweb.core.iface.InterfaceRMIdc;
@@ -18,80 +16,140 @@ import xtremweb.core.log.Logger;
 import xtremweb.core.log.LoggerFactory;
 import xtremweb.core.obj.dc.Data;
 import xtremweb.core.obj.dc.Locator;
-import xtremweb.role.examples.obj.SongBitdew;
 import xtremweb.serv.dc.DataUtil;
-import xtremweb.serv.dt.OOBTransfer;
 
 public class P2PSongs {
     /*! \example P2PSongs.java In this tutorial we will learn how to configure a P2P file
-     * sharing system using BitDew. For this experience you will need several
-     * computers connected through a TCP/IP network. Next image shows how this can be achieved using Bitdew data services: 
-     * \image html archp2p.jpg 
-     * Our P2P infrastructure consist on two types of
-     * nodes : <ul> <li> Bootstrap node , the initial connection to our P2P
-     * network, it runs a bitdew dc with a Distributed Hash Table implementation to handle a
-     * distributed song index. </li> <li> Peer node: each peer creates two
-     * Bitdew API's that differs on the data catalog they use. One of them uses a
-     * local catalog to index the locally hosted songs the peer wants to
-     * contribute. The second one references the Distributed Hash Table to query
-     * and download any song in the P2P network. </li> </ul> 
-     * <ol> 
-     *  <li> Download the last version of bitdew-stand-alone jar, choose a folder (we will call it BITDEW_HOME), and
-     *  put the jar there </li>
-     *  <li> Choose one computer as your bootstrap node and run :
-     *   @code java -jar bitdew-stand-alone-X.X.X.jar serv dc
-     *   @endcode 
-     *  </li> This command starts a distributed hash table daemon, we
-     * use <a href=
-     * "http://graal.ens-lyon.fr/~fchuffart/dokuwiki/doku.php?id=offtheshelfdlpt"
-     * >DLPT</a> 
-     * <li> In another host, download the file <a
-     * href="http://www.bitdew.net/properties_p2p.json">properties_p2p.json</a> . Let's look at
-     * two important JSON objects in this file:
-     * <ol>
-     * <li> <b>xtremweb.core.http: {port: 8080,path:"/",documentRoot:".",uploadServlet:"/fileupload",p2pServlet: "/p2pquery",bootstrapNode: "address.to.my.host.com", sshTunneling:"yes"}</b><br>
-     * The bitdew-stand-alone jar comes with an embedded JETTY web server. JSON object <b>xtremweb.core.http</b>, contains
-     * different properties needed to configure this web server, you just need to configure path,bootstrapNode and sshTunneling.   
+     * sharing system using BitDew, with this system,  you can share a folder on your machine on a P2P network, 
+     * people can find and download files from your computer, and you can make remote requests, to
+     * find and download files from the P2P network members. 
+     * 
+     * <h2> Components : </h2>
      * <ul>
-     * <li>path: root for static resources relative to BITDEW_HOME, in this case, our root will be BITDEW_HOME  </li>
-     * <li>bootstrapNode: the node where the DHT is running, if you erase this property, the default will be localhost</li>
-     * <li>sshTunneling: if you are performing the experience in a restricted network, setting "yes" to this attribute will
-     * imply that you will signal the bootstrap node later, when building the ssh tunnel</li>
+     * <li>Distributed Hash Table</li>
+     * <li>Bootstrap Node</li>
+     * <li>Peer Nodes</li>
      * </ul>
+     * <h3> The Distributed Hash Table : </h3>
+     * For this experience, bitdew use the <a href="http://graal.ens-lyon.fr/~fchuffart/dokuwiki/doku.php?id=offtheshelfdlpt">DLPT</a> <a href="http://en.wikipedia.org/wiki/Distributed_hash_table">Distributed Hash Table</a> implementation. 
+     * This component will be used as a distributed 'yellow pages' among the network members. It makes easy to find
+     * a host sharing the file we are interested on. To successfully achieve this, our yellow pages contain two types of entries.
+     * First, one word with the list of BitDew <em>Data</em> objects whose name has this word as a term. 
+     * For example, the entry 
+     * @code
+     * [ "the" , ("THE cardigans-my favorite game.mp3","THE doors-riders on THE storm.mp3","david bowie-THE man who sold the world.mp3")]. 
+     * @endcode
+     * Corresponds to a list of bitdew Data objects that contains 'the' as part of the file name.
+     * As you have seen in the previous exercises, bitdew Data objects contain the md5 file checksum too, this is the file signature, useful to uniquely identify the file on the network, and test
+     * the file has not been corrupted after the transfer. MD5 checksums make part of the other type of entries, each of them are put together with a list of IP addresses. For example, let's suppose that the file 
+     * "david bowie-the man who sold the world.mp3" has md5sum 9efe0c2a047d66685dfdc0ffffa92446, its entry on the yellow pages will be:
+     * @code
+     * [ 9efe0c2a047d66685dfdc0ffffa92446 ,  ( 234.34.56.71 , 27.23.78.99 , 134.240.123.25 )]. 
+     * @endcode
+     * This mean that a file with this md5 is actually hosted on 3 nodes, with the IP's listed above. 
+     * 
+     * With these entry types, we can find a song by first searching the file list matching our term, once the user choose a file from this list, we search according to its MD5 attribute , 
+     * which IP's have this song. Finally, with all these data, a BitDew 'GET' can be stablished and the file is downloaded from the remote peer using the HTTP protocol. In the following image
+     * you can see the event sequence, notice that the user application only talks to the API and the DHT library on the implementation layer is in charge of routing the messages according to its 
+     * internal algorithms.
+     * 
+     * \image html events.png
+     * 
+     * <h2>Node types : </h2>
+     * <h2>Bootstrap node :</h2>
+     * According to DLPT implementation, one of the nodes must be the bootstrap node, that is, the node 
+     * any peer who wants to join the network must contact first. After meeting this node, the peer can put and get information on the yellow pages, in a descentralized way,
+     * by using the DLPT internal algorithms to forward the messages to other peers. For this purpose
+     * A DLPT daemon will previously run on the bootstrap node, and it is encapsulated in the DC BitDew service.
+     * 
+     * <h2> Peer Nodes </h2>
+     * Peer nodes store physically the files to be downloaded by other peer nodes, and they download files from other network members. For this purpose they run two instances of bitdew:
+     * first instance has a DR to manage the HTTP repository to store your files, a DC to locally index their data once somebody wants to download a file, 
+     * and a DT to manage  the file transfers. The second bitdew instance references also the previously created DR and DT, but DC references the bootstrap node DHT, this instance will be used
+     * to perform researches on the P2P network. 
+     * 
+     * Next figure shows how the different node types work together : 
+     * \image html bitdewp2pArch.png
+     * 
+     * <h2> Procedure </h2>
+     * 
+     * For this experience, we will need three computers connected through a TCP/IP network, we call them pc-1, pc-2 and pc-3.
+     * <ol> 
+     *  <li> Choose one among your three machines and run there the bootstrap node, in our case we  will use pc-1. Download the latest version of bitdew-stand-alone-X.X.X.jar there, choose a folder (we will call it BITDEW_HOME), and
+     *  move the jar there</li>
+     *  
+     *  <li> DLPT library is here, please download it and move it at the same level
+     *  than the file bitdew-stand-alone-XXX.jar</li>
+     *  
+     *  <li> Download the file <a href="http://www.bitdew.net/properties_p2p.json">properties_p2p.json</a> </li>
+     *  
+     *  <li> Execute the following command
+     *  
+     *   @code 
+     *   pc-1$ java -cp sbam_standalone.jar:bitdew-stand-alone-X.X.X.jar xtremweb.role.cmdline.CommandLineTool serv --file properties_p2p.json dc
+     *   @endcode 
+     *   
+     *  This command starts the DLPT distributed hash table daemon, details on the properties_p2p.json will be explained in the next step.
+     *  
+     * <li> Take another host (pc-2 for instance), and download the last version of bitdew-stand-alone-X.X.X.jar, the DLPT jar file, and the file 
+     * <a href="http://www.bitdew.net/properties_p2p.json">properties_p2p.json</a></li>
+     * 
+     * Let's look at three important JSON objects in this file:
+     * <ol>
+     * <li> <b>xtremweb.serv.dc :  {ddc: false} </b> </li>
+     * In this object you can turn on/off the DHT, as it is a demanding process, it is turn off by default, to turn on just set the property ddc (distributed data catalog ) to true
+     * <li> <b>xtremweb.core.http: {port: 8080,path:"/",documentRoot:".",uploadServlet:"/fileupload",p2pServlet: "/p2pquery",bootstrapNode: "address.to.my.host.com"}</b><br>
+     * The bitdew-stand-alone jar comes with an embedded JETTY web server, JSON object <b>xtremweb.core.http</b>, contains
+     * different properties needed to configure it, for this case you just need to configure path and bootstrapNode.   
+     * </li>
+     * 
+     * <ul>
+     * 
+     * <li><b>path:</b> root for static resources relative to BITDEW_HOME, in this case, our root will be BITDEW_HOME  </li>
+     * 
+     * <li><b>bootstrapNode:</b> the node where the DHT is running, if you erase this property, the default will be localhost</li>
+     * 
+     * </ul>
+     * 
      * <li> <b>xtremweb.serv.dr.http:{name: "http", port:"8080",path:"songs"}</b><br>
+     * 
      * This JSON object describes to bitdew the http repository where files will be stored.
      * To make your files available, you can change "path" for the directory 
-     * where songs are stored, in our case we have them on $BITDEW_HOME/songs (remember that <em>path</em> value is relative to the BITDEW_HOME variable)</li>
+     * where songs are stored, in our case we have them on $BITDEW_HOME/songs (remember that <em>path</em> value is relative to the BITDEW_HOME variable)
+     * </li>
      * </ol>
-     * <li> Put properties-p2p.json file in the same level that
-     * bitdew-stand-alone-X.X.X.jar, and execute :
+     * 
+     * <li> Put properties-p2p.json file in the same level that bitdew-stand-alone-X.X.X.jar, and execute :
      * 
      * @code 
-     * java -jar bitdew-stand-alone-X.X.X.jar serv dc dr dt ds 
+     * pc-2$ java -cp sbam_standalone.jar:bitdew-stand-alone-X.X.X.jar xtremweb.role.examples.CommandLineTool serv --file properties_p2p.json dc dr dt ds 
      * @endcode 
      * 
      * Then in a new command line console type :
      * @code
-     * java -cp bitdew-stand-alone-X.X.X.jar xtremweb.role.examples.P2PSongs properties-p2p.json <bootstrap_node> <directory>
+     * pc-2$ java -cp sbam_standalone.jar:bitdew-stand-alone-X.X.X.jar xtremweb.role.examples.P2PSongs properties-p2p.json <bootstrap_node> <directory_to_share>
      * @endcode
-     * The following commands will: <ul> <li>Start locally the differnt dX's
-     * services</li> <li>Create two Bitdew API's (first one referencing the
-     * locally created dc and dt, second one referencing the distributed hash
-     * table on the \<bootstrap node\> <li>Register the songs contained in the
-     * local \<directory\> folder in the P2P network </li> </ul></ul> 
+     * The following commands will: 
+     * <ul> 
+     * <li>Start locally the differnt dX's services</li> 
+     * <li>Create two Bitdew API's (first one referencing the locally created dc and dt, second one referencing the distributed hash table on the \<bootstrap node\> </li>
+     * <li>Register the songs contained in the local \<directory_to_share\> folder in the P2P network </li> 
+     * </ul>
+     * 
+     * </ul> 
+     * 
      * <b>(Commented and detailed code is shown below, if you want exact details of how this things are actually performed).</b> 
-     * <li> You can
-     * repeat step 3 in as many computers as you desire, each
-     * computer will be a peer of our application </li>
+     * <li> You can repeat steps 2,3 in pc-3, and in as many computers as you desire, each computer will be a peer of our application </li>
      * 
      * <li> Now, we can request for files on the P2P network: from any host in
-     * your network open your webbrowser and type http://<bootstrap-node>:8080/findterm.html
-     * You should see a window similar to the next one (in order that this can work, port 8080 must be open in that machine) :
+     * your network open your webbrowser and type http://localhost:8080/findterm.html
+     * You should see a window similar to the next one :
      * \image html findterm.png
      * 
      * <li> Write the keyword you want to look for and click on Send</li>
      * 
-     * <li> A list similar to this one should appear : </li>
+     * <li> If there are files whose name has your word as a term, a list similar to this one should appear, if not 
+     * an empty table should appear : </li>
      * 
      * \image html results.png
      * 
@@ -282,15 +340,17 @@ public class P2PSongs {
 		 // SongBitdew is a serializable object representing the song file, it has a md5 checksum and a file name. 
 		 // We need it as the DLPT implementation receives a serializable object as second parameter.
 		 //
-		    SongBitdew sb;
+		    Data songdata = new Data();
 		    String md5 = DataUtil.checksum(file);
-		    sb = new SongBitdew(file.getName(), md5);
+		    songdata.setname(file.getName());
+		    songdata.setchecksum(md5);
+		   
 		    log.info("Putting song " + file.getName() + " with uid " + d.getuid() + "md5 is " + md5);
 		 //
 		 // we publish on the DHT  the song term associated with the object SongBitdew, so we will be able to find
 		 // the song when any of its terms is searched.  
 		 //
-		    bitdewddc.ddcPublish(toks[i], sb);
+		    bitdewddc.ddcPublish(toks[i], songdata);
 		 //
 		 // As performed in the previous step, if we request a given term, we will have a BitdewSong object <md5,file name>,
 		 // so we need to map the md5 with our local IP address, so anyone can stablish a connection with us to
