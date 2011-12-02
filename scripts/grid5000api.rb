ac262137a47efbe8cc78d1a796b813189b4f1a41
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-12-02 19:33:18 +0000

    updated
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@985 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- scripts/grid5000api.rb
+++ scripts/grid5000api.rb
@@ -1,42 +1,78 @@
 #!/usr/bin/env ruby
 require "rubygems"
 require "json"
-puts "Usage SITE RESOURCES WALLTIME NAME ENVPATH"
+
+date = ARGV[0]
+if(date.eql? "--help")
+puts "\n\n\n"
+puts " grid50000.rb makes a reservation on grid5000 using grid5000 API"
+puts " 		Usage DATE SITE RESOURCES WALLTIME NAME ENVPATH "
+puts "		DATE reservation date, put now if you want the resources now"
+puts "		SITE choose a site (grenoble, lyon , orsay etc)"
+puts "		RESOURCES a number, how many machines do you want ?"
+puts "		WALLTIME for how long ?"
+puts "		NAME job name"
+puts "		ENVPATH path to a .env file to perfom a deploy"
+exit(0);
+end
+
+if date.eql? "now" or date.eql? "NOW" then
 time = Time.new
-mysite = ARGV[0]
-resources = ARGV[1]
-walltime = ARGV[2]
-thename = ARGV[3]
-envpath = ARGV[4]
+time = time.strftime("%Y-%m-%d %H:%M:%S")
+else
+  time = date
+end
+
+mysite = ARGV[1]
+resources = ARGV[2]
+walltime = ARGV[3]
+thename = ARGV[4]
+envpath = ARGV[5]
 begin
 
   resu = "/nodes=" + resources+",walltime=" + walltime
-  time = time.strftime("%Y-%m-%d %H:%M:%S")
+  
   obj = {:name => thename, :resources => resu ,:reservation => time , :types => ["deploy"] }
     
   jsonobj = obj.to_json
   puts "json es " + jsonobj
   
   codeans = "500"
- 
+  object = {}
   
   iter = 0 
-  
-  # Submit a reservation now
-  while !(codeans == nil) and  !codeans.eql? "200" and iter < 5 
-    iter= iter + 1
-    cmnd = "curl -H \"Accept: application/json\" -H \"Content-Type: application/json\" --data '" +jsonobj + "' https://api.grid5000.fr/2.0/grid5000/sites/" + mysite +"/jobs"
-    puts "command to launch "+ cmnd
-    output =IO.popen(cmnd)do |f|
-      f.readlines
+  if (date.eql? "now" or date.eql? "NOW")
+    puts "Is a job currently submitted"
+    # Submit a reservation now
+    while !(codeans == nil) and  !codeans.eql? "200" and iter < 5 
+      iter= iter + 1
+      cmnd = "curl -H \"Accept: application/json\" -H \"Content-Type: application/json\" --data '" +jsonobj + "' https://api.grid5000.fr/2.0/grid5000/sites/" + mysite +"/jobs"
+      puts "command to launch "+ cmnd
+      output =IO.popen(cmnd)do |f|
+        f.readlines
+      end
+      object = JSON.parse output.to_s
+      puts "object is " + object.inspect
+      codeans = object["code"]
+      puts "Code answer is " + codeans.to_s + ", trying again in 3 seconds"
+      sleep(3)
     end
-    object = JSON.parse output.to_s
-    puts "object is " + object.inspect
-    codeans = object["code"]
-    puts "Code answer is " + codeans.to_s + ", trying again in 3 seconds"
-    sleep(3)
+    
+  else
+    puts "Is a job previously submitted"
+    getj = "curl https://api.grid5000.fr/2.0/grid5000/sites/"+mysite+"/jobs?user_uid=jsaray"
+    rep = IO.popen(getj)do|f|
+      f.readlines
+    end  
+    object = JSON.parse rep.to_s
+    object = object["items"][0]
+    jobid = object["uid"]
+    puts "in else jobid is #{jobid}"
+    state = object["state"]
+    puts "in else state is #{state}"
+    codeans=nil
   end
-  
+
   #The api throw nil when there are no problems, we wait until the job state is running and resources are actually allocated
   if codeans == nil
     puts "Request successful"
@@ -65,7 +101,7 @@ begin
     end
     puts "Deploying ; this will take some time"
     
-    jsondeploy = {:nodes => machines,:environment => "http://public."+mysite+".grid5000.fr/~jsaray/jsaray_squeeze.env"}
+    jsondeploy = {:nodes => machines,:environment => "http://public.grenoble.grid5000.fr/~jsaray/jsaray_squeeze.env"}
     jsondeploy = jsondeploy.to_json
     puts "Obj json deploy is #{jsondeploy}"
     
@@ -89,10 +125,7 @@ begin
       puts "in while status is #{status}" 
       sleep(5)
     end
-    
-    
-    
-    
+
     #dep = IO.popen("kadeploy3 -a "+envpath+" -f nodelist -k")do |f|
     #  f.readlines
     #end
