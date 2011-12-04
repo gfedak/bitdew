e10bbace691e0391285974bd742f79987fc98a41
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-12-04 20:07:26 +0000

    code commented and idented
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@994 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/serv/dt/bittorrent/test/BittorrentToolsTest.java
+++ src/xtremweb/serv/dt/bittorrent/test/BittorrentToolsTest.java
@@ -7,38 +7,49 @@ import org.junit.Test;
 import xtremweb.serv.dt.bittorrent.BittorrentTools;
 import junit.framework.TestCase;
 
-public class BittorrentToolsTest extends TestCase{
+/**
+ * Unit test for bittorrent tools
+ * @author jsaray
+ *
+ */
+public class BittorrentToolsTest extends TestCase {
     
-    public void setUp()
-    {
-	
+    /**
+     * Junit setup
+     */
+    public void setUp() {
+
     }
     
-    public void tearDown()
-    {
-	
+    /**
+     * Junit teardown
+     */
+    public void tearDown() {
+
     }
     
+    /**
+     * Perform a torrent make and check that it was correctly done
+     */
     @Test
-    public static void testDownloadTorrent()
-    {
-	String FILE_PATH = "/Users/josefrancisco/Downloads/lola.avi";
-	String TARGET_PATH = "/Users/josefrancisco/torrents/lola.avi.torrent";
-	BittorrentTools.startBittorrentTracker();
-	  
-	BittorrentTools.makeTorrent(FILE_PATH,TARGET_PATH);
-	
+    public static void testDownloadTorrent() {
+	String FILE_PATH = System.getProperty("user.dir") + File.separator
+		+ "bitdew.mf";
+	String TARGET_PATH = System.getProperty("user.dir") + File.separator
+		+ "bitdew.mf.torrent";
+	BittorrentTools.makeTorrent(FILE_PATH, TARGET_PATH);
+
 	assertTrue(new File(TARGET_PATH).exists());
 
     }
     
-    public static void main(String[] args)
-    {	System.setProperty("PROPERTIES_FILE","trunk-1/conf/properties.json");
+    /**
+     * Main method
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.setProperty("PROPERTIES_FILE", "trunk-1/conf/properties.json");
 	testDownloadTorrent();
     }
-    
-
- 
-   
 
 }
