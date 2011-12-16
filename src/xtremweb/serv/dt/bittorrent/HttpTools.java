dbc2d223c3ff6b9ea7c96ba66de54d60ec14ece5
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2011-12-16 17:08:49 +0000

    helper class
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1016 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/serv/dt/bittorrent/HttpTools.java
@@ -0,0 +1,44 @@
+package xtremweb.serv.dt.bittorrent;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import xtremweb.serv.dt.bittorrent.exception.HttpToolsException;
+
+public class HttpTools {
+
+    public static void getHttpFile(String filename, String url)
+	    throws HttpToolsException {
+	HttpClient httpclient = new DefaultHttpClient();
+	HttpGet httpget = new HttpGet(url);
+	HttpResponse response;
+	try {
+	    response = httpclient.execute(httpget);
+	    HttpEntity entity = response.getEntity();
+	    if (entity != null) {
+		FileOutputStream os = new FileOutputStream(filename);
+		entity.writeTo(os);
+	    } else {
+		throw new HttpToolsException(
+			"There was an error on the http response, Http response code : "
+				+ response.getStatusLine().getStatusCode()
+				+ " http reason : "
+				+ response.getStatusLine().getReasonPhrase());
+	    }
+	} catch (IOException e) {
+	    throw new HttpToolsException(
+		    "There was an error executing the GetHttp query to get the .torrent file : "
+			    + e.getMessage());
+	}
+
+    }
+
+    public static void postRequest(String url) throws HttpToolsException {
+
+    }
+
+}
