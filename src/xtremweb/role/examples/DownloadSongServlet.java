0547d1c432e2d1500246ef7188a70662b56dc210
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-01-04 14:30:53 +0000

    move to examples folder
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1056 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/role/examples/DownloadSongServlet.java
@@ -0,0 +1,130 @@
+package xtremweb.role.examples;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import xtremweb.api.bitdew.BitDewException;
+import xtremweb.api.transman.TransferManagerException;
+import xtremweb.core.com.idl.ModuleLoaderException;
+import xtremweb.core.log.Logger;
+import xtremweb.core.log.LoggerFactory;
+import xtremweb.role.examples.P2PClient;
+
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * This class builds a HTTP response when a user attempts to download a song
+ * 
+ * @author josefrancisco
+ * 
+ */
+public class DownloadSongServlet extends HttpServlet {
+
+    /**
+     * Client that encapsulate the BitDew logic to perform a search
+     * in a P2P network
+     */
+    private P2PClient client;
+
+    /**
+     * Logging
+     */
+    protected Logger log = LoggerFactory.getLogger("DownloadSongServlet");
+
+    /**
+     * Class constructor
+     */
+    public DownloadSongServlet() {
+	client = new P2PClient();
+    }
+
+    /**
+     * Given a song name and his md5 signature, stablish a connection with the
+     * host who has this song and download it
+     * 
+     * @param request
+     *            http request containing songname and md5 signature
+     * @param response
+     *            the song file
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	String directory = request.getParameter("directory");
+	String ip = request.getParameter("ip");
+	try {
+	    if (request.getParameter("md5") != null
+		    && request.getParameter("songname") != null) {
+		String md5 = request.getParameter("md5");
+		log.debug("md5 is " + md5);
+		String songname = request.getParameter("songname");
+		client.download(songname, md5, ip);
+		client.republish(songname, md5);
+	    }
+	    String answer = "<table><tr><td>File name</td><td>MD5</td></tr>";
+	    File dir = new File(directory);
+	    if (dir.exists()) {
+		File[] file = dir.listFiles();
+		for (int i = 0; i < file.length; i++) {
+		    String item = file[i].getName();
+		    byte[] b = new byte[(int) file[i].length()];
+		    answer += "<tr>";
+		    if (!file[i].isDirectory()) {
+			FileInputStream fis = new FileInputStream(file[i]);
+			fis.read(b);
+			MessageDigest md5dig;
+			md5dig = MessageDigest.getInstance("MD5");
+			byte[] bytes = md5dig.digest(b);
+			String hexw = new String(Hex.encodeHex(bytes));
+			answer += "<td>" + item + "</td><td>" + hexw + "</td>";
+		    }
+		    answer += "</tr>";
+		}
+		answer += "</table>";
+		response.getWriter().println(answer);
+
+		// Seed the file in the P2P network.
+	    } else {
+		response.getWriter()
+			.println(
+				"<p>That directory do not exist in your file system !!</p>");
+	    }
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_OK);
+	} catch (BitDewException e) {
+	    e.printStackTrace();
+	    response.getWriter().println("<p>"+e.getMessage()+"</p>");
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	} catch (TransferManagerException e) {
+	    e.printStackTrace();
+	    response.getWriter().println("<p>"+e.getMessage()+"</p>");
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	} catch (ModuleLoaderException e) {
+	    e.printStackTrace();
+	    response.getWriter().println("<p>"+e.getMessage()+"</p>");
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	} catch (NoSuchAlgorithmException e) {
+	    e.printStackTrace();
+	    response.getWriter().println("<p>"+e.getMessage()+"</p>");
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	}
+
+    }
+
+    /**
+     * Http POST method is not used for the moment
+     */
+    public void doPost() {
+    }
+
+}
---
+++ src/xtremweb/role/examples/P2PServlet.java
@@ -0,0 +1,173 @@
+package xtremweb.role.examples;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import xtremweb.api.bitdew.BitDew;
+import xtremweb.api.bitdew.BitDewException;
+import xtremweb.core.com.idl.ComWorld;
+import xtremweb.core.com.idl.ModuleLoaderException;
+import xtremweb.core.conf.ConfigurationException;
+import xtremweb.core.conf.ConfigurationProperties;
+import xtremweb.core.iface.InterfaceRMIdc;
+import xtremweb.core.iface.InterfaceRMIdr;
+import xtremweb.core.iface.InterfaceRMIds;
+import xtremweb.core.iface.InterfaceRMIdt;
+import xtremweb.core.log.Logger;
+import xtremweb.core.log.LoggerFactory;
+import xtremweb.core.obj.dc.Data;
+
+/**
+ * This class builds a HTTP response with the list of songs matching a specific
+ * term entered by the user
+ * 
+ * @author josefrancisco
+ * 
+ */
+public class P2PServlet extends HttpServlet {
+
+    /**
+     * Data catalog
+     */
+    private InterfaceRMIdc ddc;
+
+    /**
+     * Data repository
+     */
+    private InterfaceRMIdr dr;
+
+    /**
+     * Data scheduler
+     */
+    private InterfaceRMIds ds;
+
+    /**
+     * Data Transfer
+     */
+    private InterfaceRMIdt dt;
+
+    /**
+     * Bitdew API
+     */
+    private BitDew bd;
+
+    /**
+     * Logger
+     */
+    protected Logger log = LoggerFactory.getLogger("P2PServlet");
+
+    private String BOOTSTRAP_NODE;
+    private boolean SSH_TUNNELING;
+
+    /**
+     * Constructor
+     */
+    public P2PServlet() {
+	try {
+	    Properties props = ConfigurationProperties.getProperties();
+	    String ssht = (String) props
+		    .getProperty("xtremweb.core.http.sshTunneling");
+	    String bootstrapnode = (String) props
+		    .getProperty("xtremweb.core.http.bootstrapNode");
+	    BOOTSTRAP_NODE = (bootstrapnode != null) ? bootstrapnode
+		    : InetAddress.getLocalHost().getHostAddress();
+	    String LOCAL_ADDRESS = InetAddress.getLocalHost().getHostAddress();
+	    ddc = (InterfaceRMIdc) ComWorld.getComm(BOOTSTRAP_NODE, "RMI",
+		    4325, "dc");
+	    dr = (InterfaceRMIdr) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dr");
+	    dt = (InterfaceRMIdt) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "dt");
+	    ds = (InterfaceRMIds) ComWorld.getComm(LOCAL_ADDRESS, "RMI", 4325,
+		    "ds");
+	    bd = new BitDew(ddc, dr, dt, ds);
+
+	} catch (ModuleLoaderException e) {
+	    log.warn("All bitdew services could not be loaded, if you want to use BitDew API make sure you launch them before "
+		    + e.getMessage());
+	} catch (UnknownHostException e) {
+	    log.info("There was an exception !! " + e.getMessage());
+	} catch (ConfigurationException e) {
+	    log.info("there was an exception !! " + e.getMessage());
+	}
+    }
+
+    /**
+     * Returns a list of songs matching a term given by the user
+     * 
+     * @request HTTP request with a user-specified term
+     * @response The response the servlet will produce
+     */
+    protected void doGet(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+	String HOST_NAME = InetAddress.getLocalHost().getHostName();
+	String param = request.getParameter("term");
+	log.debug("parameter term : " + param);
+	log.debug("bootstrap node value is " + BOOTSTRAP_NODE);
+	response.setContentType("xml");
+	String responsexml = "<table border=\"1\"><tr><td>Download</td><td>Song Name</td><td>MD5</td><td>Owner(s)</td></tr>";
+	List l;
+	try {
+	    log.info("bd is 3" + bd);
+	    l = bd.ddcSearch(param);
+	    log.debug(" size of l is " + l.size());
+	    // for each result write it on a html table
+	    for (int i = 0; i < l.size(); i++) {
+		String md5 = ((Data) l.get(i)).getchecksum();
+		responsexml += "<tr>"
+			+ "<td><input type=\"checkbox\" name=\"checkbox" + i
+			+ "\"/></td>" + "<td>" + ((Data) l.get(i)).getname()
+			+ "</td>" + "<td>" + md5 + "</td>" + "<td>"
+			+ getIps(md5) + "</td>" + "</tr>";
+	    }
+	    responsexml += "</table>";
+	    response.getWriter().println(responsexml);
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_OK);
+	} catch (BitDewException e) {
+	    e.printStackTrace();
+	    response.getWriter()
+		    .println(
+			    "<table border=\"1\">"
+				    + "<tr><td>Download</td><td>Song Name</td><td>MD5</td><td>Owner(s)</td></tr>"
+				    + "<tr><td>There was a problem executing your request "
+				    + e.getMessage() + "</td>"
+				    + "<td></td><td></td><td></td></tr>"
+				    + "</table>");
+	    response.setContentType("text/html");
+	    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	}
+    }
+
+    private String getIps(String md5) throws BitDewException {
+	String responsexml = "";
+	List ips;
+	ips = bd.ddcSearch(md5);
+	HashSet s = new HashSet();
+	s.addAll(ips);
+	for (Iterator iterator = s.iterator(); iterator.hasNext();) {
+	    String object = (String) iterator.next();
+	    responsexml += object + ", ";
+	}
+	return responsexml;
+    }
+
+    /**
+     * Http POST method is not used for the moment
+     */
+    protected void doPost(HttpServletRequest request,
+	    HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
