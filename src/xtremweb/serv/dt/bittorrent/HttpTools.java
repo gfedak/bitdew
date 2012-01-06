859cdd499d8f267cbcf8ee6964b6d7a6f8ab5a34
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-01-06 17:39:15 +0000

    added postfilehttp method to facotize code
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1073 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/serv/dt/bittorrent/HttpTools.java
+++ src/xtremweb/serv/dt/bittorrent/HttpTools.java
@@ -1,25 +1,36 @@
 package xtremweb.serv.dt.bittorrent;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import xtremweb.serv.dt.bittorrent.exception.HttpToolsException;
 
 /**
  * Helper class to handle .torrent file transfers through http
+ * 
  * @author josefrancisco
- *
+ * 
  */
 public class HttpTools {
-    
+
     /**
      * Get a filename through http
-     * @param filename the filename
-     * @param url the url where the file is
+     * 
+     * @param filename
+     *            the filename
+     * @param url
+     *            the url where the file is
      * @throws HttpToolsException
      */
     public static void getHttpFile(String filename, String url)
@@ -48,8 +59,37 @@ public class HttpTools {
 
     }
 
-    public static void postRequest(String url) throws HttpToolsException {
+    public static void postFileHttp(String fileName, String url)
+	    throws HttpToolsException {
+	try {
+	    HttpClient httpclient = new DefaultHttpClient();
 
+	    HttpPost httppost = new HttpPost(url);
+
+	    FileBody bin = new FileBody(new File(fileName));
+	    StringBody comment = new StringBody("Filename: " + fileName);
+
+	    MultipartEntity reqEntity = new MultipartEntity();
+	    reqEntity.addPart("bin", bin);
+	    reqEntity.addPart("comment", comment);
+	    httppost.setEntity(reqEntity);
+
+	    HttpResponse response = httpclient.execute(httppost);
+	    if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK)
+		throw new HttpToolsException(
+			"There was an error on the http response, Http response code : "
+				+ response.getStatusLine().getStatusCode()
+				+ " http reason "
+				+ response.getStatusLine().getReasonPhrase());
+	} catch (ClientProtocolException e) {
+	    throw new HttpToolsException(
+		    "There was an error executing the file POST request "
+			    + e.getMessage());
+	} catch (IOException e) {
+	    throw new HttpToolsException(
+		    "There was an error executing the file POST request "
+			    + e.getMessage());
+	}
     }
 
 }
