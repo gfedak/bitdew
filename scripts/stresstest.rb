4bfb2df05389f14c17c757285ad44e9eccd54ba3
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-01-19 18:29:01 +0000

    stress test for get nbitdew function
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1094 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ scripts/stresstest.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/env ruby
+require "rubygems"
+require 'net/ssh'
+version = "0.2.8"
+IO.popen("taktuk -d-1 -f nodelist broadcast exec { 'killall java' }")do |f|
+    f.readlines
+end
+puts "Erasing all"
+IO.popen("taktuk -d-1 -f nodelist broadcast exec { 'rm -rf *' }")do |f|
+   f.readlines
+end
+#%x("taktuk -d-1 -f nodelist broadcast exec { 'killall java' }")
+
+
+IO.popen("taktuk -d-1 -f nodelist broadcast put { /home/jsaray/bitdew-stand-alone-"+version+".jar } { /home/jsaray/bitdew-stand-alone-"+version+".jar }")do |f|
+ f.readlines
+end
+
+#%x("taktuk -d-1 -f nodelist broadcast put { /home/jsaray/props.json } { /home/jsaray/props.json }")
+IO.popen("taktuk -d-1 -f nodelist broadcast put { /home/jsaray/props.json } { /home/jsaray/props.json }")do |f|
+    f.readlines
+end
+#uid = ARGV[0]
+#stable_node = ARGV[1]
+number_of_threads = ARGV[0]
+filename = ARGV[1]
+
+i = 0
+machines = IO.readlines("nodelist")
+services = machines[0].slice(/[^\n]*/)
+
+puts "Services machine is "  + services
+
+putmachine = machines[1].slice(/[^\n]*/i)
+
+puts "Put machine is " + putmachine
+
+stable_node = services
+IO.popen("taktuk -d-1 -f nodelist broadcast put { /home/jsaray/helloworld } { /home/jsaray/helloworld }") do |f|
+f.readlines
+end
+
+uid = ""
+Net::SSH.start(services,"jsaray") do |ssh|
+    ssh.exec "nohup java -jar bitdew-stand-alone-" + version + ".jar --file "+filename+" serv dc dt dr ds > servout 2> serverr & "
+end
+sleep(120)
+
+Net::SSH.start(putmachine,"jsaray") do |ssh|
+    output = ssh.exec! "nohup java -jar bitdew-stand-alone-"+version+".jar --file "+filename+" --host " + services +" put helloworld > putout 2> puterr "
+    IO.popen("scp " + putmachine +":putout .") do|f|
+	    f.readlines
+    end
+    uid = IO.readlines("putout")[0]
+    puts "The id line is #{uid}"
+    uid = uid.match(/[[a-fA-F0-9-]*]/)[0]
+    uid = uid[1,uid.length-2]
+    puts "The uid is  #{uid}"
+end
+
+machines = machines[2 .. machines.length - 1]
+machines.each{|machine|
+i=0
+machine = machine.slice(/[^\n]*/)
+puts "Executing on machine " + machine
+while i < number_of_threads.to_i do
+Net::SSH.start(machine,"jsaray") do |ssh|
+#    while i < number_of_threads.to_i do
+  puts "while"
+        ssh.exec "nohup java -cp bitdew-stand-alone-"+version+".jar xtremweb.role.cmdline.TestGetMultiple " + stable_node + " "+ uid + " " + i.to_s + " > out"+i.to_s + " 2> err"+i.to_s+" &"
+        i = i + 1
+
+#    end
+puts "finish"
+end
+end
+
+}
+out = ""
+puts "File is downloading on each node"
+sleep(60)
+machines.each{|machine|
+machine = machine.slice(/[^\n]*/)
+Net::SSH.start(machine,"jsaray") do |ssh|
+
+out = ssh.exec!("ls -al | grep fofis")
+end
+puts "Exit on  node "+ machine.to_s  + " " + out.to_s
+}
