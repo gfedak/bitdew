4e5cd838544a1bc61e555fda428a3fbb45b47b8f
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-02-24 16:17:27 +0000

    test case for xml interface
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1142 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/serv/dt/jsaga/test/XMLTest.java
@@ -0,0 +1,41 @@
+package xtremweb.serv.dt.jsaga.test;
+
+import org.junit.Test;
+
+import xtremweb.serv.dt.jsaga.xml.XMLException;
+import xtremweb.serv.dt.jsaga.xml.XmlDomImpl;
+import junit.framework.TestCase;
+
+public class XMLTest extends TestCase{
+    
+
+    private XmlDomImpl dom;
+    
+    public void setUp(){
+	try {
+	    dom = new XmlDomImpl("VOMS","/Applications/JSAGA/etc/jsaga-default-contexts.xml");
+	} catch (XMLException e) {
+	    System.out.println("se totio");
+	    e.printStackTrace();
+	    fail();
+	}
+    }
+    
+    @Test
+    public void testXML(){
+	try {
+	    assertEquals(dom.getElement("Server"),"voms://cclcgvomsli01.in2p3.fr:15011/O=GRID-FR/C=FR/O=CNRS/OU=CC-IN2P3/CN=cclcgvomsli01.in2p3.fr");
+	
+	assertEquals(dom.getElement("UserCert"),"/Users/josefrancisco/usercert.pem");
+	assertEquals(dom.getElement("UserKey"),"/Users/josefrancisco/userkey.pem");
+	assertEquals(dom.getElement("LifeTime"),"PT24H");
+	} catch (XMLException e) {
+	    fail();
+	}
+    }
+    
+    public void tearDown(){
+	dom=null;
+    }
+
+}
