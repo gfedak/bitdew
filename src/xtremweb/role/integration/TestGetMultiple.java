4fc44d9a025f3d983227a5d56fbbedeaead55539
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-03-12 15:39:58 +0000

    commented
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1159 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/role/integration/TestGetMultiple.java
+++ src/xtremweb/role/integration/TestGetMultiple.java
@@ -3,23 +3,44 @@ package xtremweb.role.integration;
 import xtremweb.core.log.Logger;
 import xtremweb.core.log.LoggerFactory;
 
+/**
+ * This class must be executed in a different java process each time it is called.
+ * Its purpose is to put some stress in a remote Transfer Manager invoking the bitdew get command concurrently.
+ * In order to understand more about this class, please read the stresstest.rb script in scripts folder.
+ * @author josefrancisco
+ *
+ */
 public class TestGetMultiple {
     
+    private static String GETFNAME="getfile";
+    /**
+     * Logger class
+     */
     private static Logger log = LoggerFactory.getLogger("TestGetMultiple");
     
-    public void go(String sn,String uid,int tam)
-    {	String STABLE_NODE = sn;
-	//for ( int i =0; i < tam;i++)
-	//{
-	    ThreadGet tg = new ThreadGet(STABLE_NODE,uid,tam);
-	    tg.execute();
-	//}
+    /**
+     * 
+     * @param sn
+     * @param uid
+     * @param tam
+     */
+    public void go(String sn,String uid,int tam,String getfname)
+    {	String STABLE_NODE = sn;	
+	GetProcess tg = new GetProcess(STABLE_NODE,uid,tam,getfname);
+	tg.execute();
 	log.info("finish main");
     }
     
+    /**
+     * Main method
+     * @param args an array containing the following parameters : 
+     * args[0] node where bitdew services are running
+     * args[1] data uid that you want to download
+     * args[2] Application-defined process-id 
+     */
     public static void main(String[] args){
 	log.setLevel("debug");
-	new TestGetMultiple().go(args[0],args[1],Integer.parseInt(args[2]));
+	new TestGetMultiple().go(args[0],args[1],Integer.parseInt(args[2]),GETFNAME);
     }
 
 }
