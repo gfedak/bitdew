5fa76853b35ae97bd898fffcc77e44a0c09f95a9
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-08-04 18:26:33 +0000

    exception control in servlet side
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1316 7f936411-ad0f-0410-9f4a-d8ad0d536174

---
+++ src/xtremweb/core/http/exception/RemoteFileNotSpecifiedException.java
@@ -0,0 +1,8 @@
+package xtremweb.core.http.exception;
+
+public class RemoteFileNotSpecifiedException extends Exception {
+	
+	public RemoteFileNotSpecifiedException(String msg){
+		super(msg);
+	}
+}
