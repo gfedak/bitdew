7cedd0aebb722ed0938f76c019b8c201d92a1082
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-08-13 15:54:41 +0000

    documented
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1330 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/core/http/HttpServerFactory.java
+++ src/xtremweb/core/http/HttpServerFactory.java
@@ -1,18 +1,24 @@
 package xtremweb.core.http;
 
 /**
- * Describe class HttpServerFactory here.
- *
- *
+ * This class creates a http server using the factory pattern
  * Created: Fri Aug 24 14:28:31 2007
  *
  * @author <a href="mailto:fedak@lri7-234.lri.fr">Gilles Fedak</a>
  * @version 1.0
  */
 public class HttpServerFactory {
-
+    
+    /**
+     * http server interface
+     */
     public static HttpServer httpServer = null;;
-
+    
+    /**
+     * Main method to retrieve a http server
+     * @return
+     * @throws Exception
+     */
     public static HttpServer getHttpServer() throws Exception {
 	if (httpServer == null) {
 	    httpServer = new HttpServer();
