0f48f2f8542b696a61ae48ecac377a4b9f01357f
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-08-13 15:52:28 +0000

    commented code
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1329 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- src/xtremweb/api/activedata/ActiveDataCallback.java
+++ src/xtremweb/api/activedata/ActiveDataCallback.java
@@ -4,18 +4,27 @@ import xtremweb.core.obj.dc.Data;
 import xtremweb.core.obj.ds.Attribute;
 
 /**
- * Describe class ActiveDataCallback here.
- *
- *
+ * This interface describe the methods that should be called in a callback.
+ * Currently only two methods are supported : onDataScheduled, on DataDeleted
  * Created: Thu Dec 27 21:46:25 2007
  *
  * @author <a href="mailto:fedak@xtremciel.gillus.net">Gilles Fedak</a>
  * @version 1.0
  */
 public interface ActiveDataCallback {
-
+    
+    /**
+     * Triggered when a data is scheduled from the data scheduler to the client cache
+     * @param data the data that was scheduled
+     * @param attr the attribute that was scheduled
+     */
     public  void onDataScheduled(Data data, Attribute attr);
-
+    
+    /**
+     * Triggered when a data is erased from the client data cache
+     * @param data the data tha was deleted
+     * @param attr the attribute
+     */
     public  void onDataDeleted(Data data, Attribute attr);
 
 }
