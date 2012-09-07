3848cac2481c4bd2cf6e65a3322350024dbb394c
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-09-07 16:08:06 +0000

    small changes to make the files easy to execute
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1371 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- scripts/stresstest.rb
+++ scripts/stresstest.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require "rubygems"
 require 'net/ssh'
-version = "0.2.8"
+version = "1.1.1"
 
 # 
 # 
@@ -12,12 +12,15 @@ version = "0.2.8"
 # ARGV[0] the number of threads per machine that you want to perform a GET. 
 # ARGV[1] file name that you want to put/get
 # Prerrequisites : a file named "nodelist" must be in the same directory where is this script. Each file line must be a host name.
+# you will also need, a properties file, you can use the one in trunk-1/scripts/configuration_files/stresstest.json 
 # The first line will be a stable node where bitdew services will run.
 # The second line is the node where the bitdew put will be executed
 # The rest of lines will be workers that perform a get command.
 # To verify that the test was successful, at the end the script perform a ls on each machine. You must see ARGV[0] files with name file_i
 # with the same size than ARGV[1].
-TIMEOUT=120
+# Please not that the total number of files that will appear is (number_of_nodes - 2)*number_of_threads_per_core
+
+TIMEOUT=210
 BASE_FOLDER = "/home/jsaray/"
 REMOTE_FOLDER = "/home/jsaray/"
 PROPERTIES_FILE = "stresstest.json"
@@ -87,22 +90,28 @@ Net::SSH.start(putmachine,"jsaray") do |ssh|
     end
     uid = IO.readlines("putout")[0]
     puts "The id line is #{uid}"
-    uid = uid.match(/[[a-fA-F0-9-]*]/)[0]
+    uid = uid.match(/\[[a-fA-F0-9-]*\]/)[0]
     uid = uid[1,uid.length-2]
     puts "The uid is  #{uid}"
 end
-
+threadgets = []
 machines = machines[2 .. machines.length - 1]
 machines.each{|machine|
-   i=0
+   
    machine = machine.slice(/[^\n]*/)
    puts "Executing on machine " + machine
-   while i < number_of_threads.to_i do
+   
+   threadget = Thread.new {
+      i=0
       Net::SSH.start(machine,"jsaray") do |ssh|
-         ssh.exec "nohup java -cp bitdew-stand-alone-"+version+".jar xtremweb.role.integration.TestGetMultiple " + stable_node + " "+ uid + " " + i.to_s + " > out"+i.to_s + " 2> err"+i.to_s+" &"
-         i = i + 1
-      end
-   end
+         while i < number_of_threadsi.to_i do
+            ssh.exec "nohup java -cp bitdew-stand-alone-"+version+".jar xtremweb.role.integration.TestGetMultiple " + stable_node + " "+ uid + " " + i.to_s + " > out"+i.to_s + " 2> err"+i.to_s+" &"
+	 i = i + 1
+         end
+      end      
+   }
+   threadgets << threadget
+   
 }
 out = ""
 puts "File is downloading on each node"
