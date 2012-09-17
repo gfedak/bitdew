5cf92f8388876c03850080cdeeb754a5671c154e
jose <jose@7f936411-ad0f-0410-9f4a-d8ad0d536174>
2012-09-17 09:40:43 +0000

    updated and commented
    git-svn-id: https://scm.gforge.inria.fr/svn/bitdew/trunk-1@1400 7f936411-ad0f-0410-9f4a-d8ad0d536174

--- scripts/p2p.rb
+++ scripts/p2p.rb
@@ -1,10 +1,15 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'net/ssh'
-version="0.2.8"
+version="1.1.2"
 
 # This SCRIPT splits equally files on a given folder in your localhost to N different hosts
-
+# You need properties_bootstrap.json file, it has configuration properties related to bootstrap
+# You need properties_peer.json file, it has configuration properties related to each peer, follow the instructions on
+# BitDew web site to understand the content of both files.
+# You need a nodelist file, the first line will be the bootstrap node, the rest peers
+# Finally you need a folder with files that you want to share. The first argument you give to this script is the folder
+# name, second argument is the machine number you want to share the files and the third argument how many files per machine.
 
 #allhosts will store the Grid500 OAR_NODEFILE content 
 iout = IO.popen("uniq nodelist")
@@ -28,28 +33,31 @@ end
 puts "allhosts name is " + allhosts.class.name  
 nodes = IO.readlines("/home/jsaray/nodelist")
 centralnode = nodes[0] 
-term = nodes[1]
-term = term.slice(/[^\n]*/)
-arr = nodes[1,(nodes.length-1)]
+
+
+arr = nodes[1,offset]
 puts "centralnode is " + centralnode
 killjava = %x(taktuk -d-1 -f nodelist broadcast exec { killall java })
 output = %x(taktuk -d-1 -f nodelist broadcast exec { 'rm -rf *'  })
-mkdir = %x(taktuk -d-1 -f nodelist broadcast exec { mkdir songs })
-puts mkdir
+IO.popen("taktuk -d-1 -f nodelist broadcast exec { mkdir " + path + " }")do |f|
+    f.readlines
+end
+#puts mkdir
 #the executable code is sent to different peers
 #%x(taktuk -d-1 -f nodelist broadcast put { /home/jsaray/bitdewp2pn.jar } { /home/jsaray/bitdewp2pn.jar })
 %x(taktuk -d-1 -f nodelist broadcast put { /home/jsaray/sbam_standalone.jar } { /home/jsaray/sbam_standalone.jar })
 IO.popen("taktuk -d-1 -f nodelist broadcast put { /home/jsaray/bitdew-stand-alone-"+version+".jar } { /home/jsaray/bitdew-stand-alone-"+version+".jar }")do |f|
   f.readlines
 end
+%x(taktuk -d-1 -f nodelist broadcast put { /home/jsaray/properties_bootstrap.json } { /home/jsaray/properties_bootstrap.json })
 centralnode = centralnode.slice(/[^\n]*/)
 #services are started on central node, special attention is dc, which contains a ddc and the DHT 
 Net::SSH.start(centralnode,"jsaray") do |ssh|
-  ssh.exec "java -cp sbam_standalone.jar:bitdew-stand-alone-"+version+".jar xtremweb.role.cmdline.CommandLineTool serv dc dt ds dr > /home/jsaray/servout"+centralnode+" 2> /home/jsaray/serverr"+centralnode+" &"
+  ssh.exec "java -cp sbam_standalone.jar:bitdew-stand-alone-"+version+".jar xtremweb.role.cmdline.CommandLineTool --file properties_bootstrap.json serv dc dt ds dr > /home/jsaray/servout"+centralnode+" 2> /home/jsaray/serverr"+centralnode+" &"
 end
 puts "Waiting for services launching"
 sleep(7)
-%x(taktuk -d-1 -f nodelist broadcast put { /home/jsaray/properties.json } { /home/jsaray/properties.json })
+%x(taktuk -d-1 -f nodelist broadcast put { /home/jsaray/properties_peer.json } { /home/jsaray/properties_peer.json })
 allfiles = Dir.entries(path)
 puts "allfiles content " + allfiles.to_s
 puts "allfiles size is " + allfiles.length.to_s
@@ -57,19 +65,19 @@ puts "allfiles sub 5 is " + allfiles.to_s
 puts "arr content is " + arr.to_s
 posi = 2
 #the files on directory are sent in blocks of signaled by variable filesperhost
-arr[1..offset].each{|line|   
+arr.each{|line|   
     line = line.slice(/[^\n]*/)
     
 
     Net::SSH.start(line,"jsaray") do |ssh|
-        ssh.exec "nohup java -cp sbam_standalone.jar:bitdew-stand-alone-"+version+".jar xtremweb.role.cmdline.CommandLineTool -v --file properties.json serv dc dt dr ds > /home/jsaray/outserv" + line + " 2> /home/jsaray/errserv" + line + " &"
+        ssh.exec "nohup java -cp sbam_standalone.jar:bitdew-stand-alone-"+version+".jar xtremweb.role.cmdline.CommandLineTool -v --file properties_peer.json serv dc dt dr ds > /home/jsaray/outserv" + line + " 2> /home/jsaray/errserv" + line + " &"
     end
     sleep(3)
 
 
     allfiles[posi..posi+filesperhost-1].each{|file|        
         puts "sending file " + file +" to host " + line
-	IO.popen("scp \"" +path+"/"+ file +  "\" " + line+":songs/")do|f|
+	IO.popen("scp \"" +path+"/"+ file +  "\" " + line+":" + path + "/")do|f|
             f.readlines
         end
     }
@@ -82,8 +90,8 @@ arr = nodes[1,(nodes.length-1)]
          line = line.slice(/[^\n]*/)
          puts "Put on line " + line
          Net::SSH.start(line,"jsaray")do |ssh|
-         ssh.exec "nohup java -cp sbam_standalone.jar:bitdew-stand-alone-"+version+".jar xtremweb.role.examples.P2PSongs properties.json " + centralnode + " /home/jsaray/songs > /home/jsaray/out"+line+" 2> /home/jsaray/err"+line+" &"
+         ssh.exec "nohup java -cp sbam_standalone.jar:bitdew-stand-alone-"+version+".jar xtremweb.role.examples.P2PSongs properties_peer.json " + centralnode + " /home/jsaray/songs > /home/jsaray/out"+line+" 2> /home/jsaray/err"+line+" &"
           end
 }
 puts "Waiting for Put called in all nodes"
-sleep(15)
+sleep(30)
